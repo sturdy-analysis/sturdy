5ef19aa7af7faf90b0a625e73116ac5625df125e
Sebastian Erdweg <seba.github@gmail.com>
2021-07-13 09:53:01 +0200

    refactored value representations

--- src/main/scala/stateful/whilelang/Random.scala
+++ src/main/scala/stateful/whilelang/Random.scala
@@ -1,17 +1,15 @@
 package stateful.whilelang
 
-import scala.util.Random
-
 trait Random[V] {
   def randomNum(): V
 }
 
 trait RandomImpl extends Random[ValImpl.Value] {
   import ValImpl._
-  override def randomNum(): Value = DoubleValue(Random.nextDouble())
+  override def randomNum(): Value = DoubleValue(util.Random.nextDouble())
 }
 
 trait RandomAbs extends Random[ValAbs.Value] {
   import ValAbs._
-  override def randomNum(): Value = NumValue(None)
+  override def randomNum(): Value = NumValue(Actual(Interval(0, 1)))
 }
--- src/main/scala/stateful/whilelang/Val.scala
+++ src/main/scala/stateful/whilelang/Val.scala
@@ -60,40 +60,29 @@ object ValAbs {
   case object TopValue extends Value {
     override def toString: String = "Top"
   }
-  case class BoolValue(v: Option[Boolean]) extends Value {
+  case class BoolValue(v: Topped[Boolean]) extends Value {
     def join(other: BoolValue): BoolValue =
-      if (this == other) this
-      else BoolValue(None)
+      BoolValue(for (b1 <- v; b2 <- other.v if b1 == b2) yield b1)
     def &&(other: BoolValue): BoolValue = BoolValue(for (b1 <- v; b2 <- other.v) yield b1 && b2)
     def ||(other: BoolValue): BoolValue = BoolValue(for (b1 <- v; b2 <- other.v) yield b1 || b2)
     def ! : BoolValue = BoolValue(for (b1 <- v) yield !b1)
-    def same(other: BoolValue): BoolValue = BoolValue(for (b1 <- v; b2 <- other.v) yield b1 == b2)
-    override def toString: String = if (v.isEmpty) "TopBool" else v.get.toString
+    override def toString: String = v.toString("Bool")
   }
-  case class NumValue(v: Option[Interval]) extends Value {
-    def join(other: NumValue): NumValue = {
-      if (this.v.isEmpty || other.v.isEmpty)
-        NumValue(None)
-      else if (this.v.get same other.v.get)
-        this
-      else NumValue(Some(this.v.get.join(other.v.get)))
-    }
-    def topmap(f: (Interval, Interval) => Interval, v2: NumValue): NumValue =
-      NumValue(for (iv1 <- v; iv2 <- v2.v) yield f(iv1, iv2))
-    override def toString: String = if (v.isEmpty) "TopNum" else v.get.toString
+  case class NumValue(v: Topped[Interval]) extends Value {
+    def join(other: NumValue): NumValue =
+      NumValue(for (iv1 <- v; iv2 <- other.v) yield iv1.join(iv2))
+    override def toString: String = v.toString("Num")
   }
 
   case class Interval(l: Double, h: Double) {
+    if (l > h) throw new IllegalArgumentException(s"Empty intervals are illegal $this")
+
     def join(other: Interval): Interval =
-      if (this.isEmpty) other
-      else if (other.isEmpty) this
-      else Interval(Math.min(l, other.l), Math.max(h, other.h))
-    def isEmpty: Boolean = l > h
+      Interval(Math.min(l, other.l), Math.max(h, other.h))
     def +(y: Interval): Interval = Interval(l + y.l, h + y.h)
     def -(y: Interval): Interval = Interval(l - y.l, h - y.h)
     def *(y: Interval): Interval = withBounds2(_*_, y)
     def /(y: Interval): Interval = withBounds2(_/_, y)
-    def same(y: Interval): Boolean = this.isEmpty && y.isEmpty || l == y.l && h == y.h
     def withBounds2(f: (Double, Double) => Double, that: Interval): Interval = {
       val v1 = f(this.l, that.l)
       val v2 = f(this.l, that.h)
@@ -103,6 +92,22 @@ object ValAbs {
       val high = Math.max(v1, Math.max(v2, Math.max(v3, v4)))
       Interval(low, high)
     }
+    def <(that: Interval): Topped[Boolean] =
+      if (this.h < that.l)
+        Actual(true)
+      else if (that.h <= this.l)
+        Actual(false)
+      else
+        Top
+    def ===(that: Interval): Topped[Boolean] = {
+      if (this.l == this.h && this.l == that.l && this.h == that.h)
+        Actual(true)
+      else if (this.h < that.l || that.h < this.l)
+        Actual(false)
+      else
+        Top
+    }
+
     override def toString: String = s"[$l,$h]"
   }
 
@@ -117,7 +122,7 @@ object ValAbs {
 trait ValAbs extends Val[ValAbs.Value] with JoinComputation {
   import ValAbs._
   override type ValJoin[A] = Join[A]
-  override def boolLit(b: Boolean, l: Label): Value = BoolValue(Some(b))
+  override def boolLit(b: Boolean, l: Label): Value = BoolValue(Actual(b))
   override def and(e1: Value, e2: Value, l: Label): Value = (e1, e2) match {
     case (TopValue, _) => TopValue
     case (_, TopValue) => TopValue
@@ -129,52 +134,82 @@ trait ValAbs extends Val[ValAbs.Value] with JoinComputation {
     case b: BoolValue => b.!
     case _ => throw new IllegalArgumentException(s"Expected Bool but got $e")
   }
-  override def numLit(n: Double, l: Label): Value = NumValue(Some(Interval(n, n)))
+  override def numLit(n: Double, l: Label): Value = NumValue(Actual(Interval(n, n)))
   override def add(e1: Value, e2: Value, l: Label): Value = (e1, e2) match {
     case (TopValue, _) => TopValue
     case (_, TopValue) => TopValue
-    case (n1: NumValue, n2: NumValue) => n1.topmap(_+_, n2)
+    case (n1: NumValue, n2: NumValue) =>
+      NumValue(for (iv1 <- n1.v; iv2 <- n2.v) yield iv1 + iv2)
     case _ => throw new IllegalArgumentException(s"Expected two Interval but got ($e1, $e2)")
   }
   override def sub(e1: Value, e2: Value, l: Label): Value = (e1, e2) match {
     case (TopValue, _) => TopValue
     case (_, TopValue) => TopValue
-    case (n1: NumValue, n2: NumValue) => n1.topmap(_-_, n2)
+    case (n1: NumValue, n2: NumValue) =>
+      NumValue(for (iv1 <- n1.v; iv2 <- n2.v) yield iv1 - iv2)
     case _ => throw new IllegalArgumentException(s"Expected two Interval but got ($e1, $e2)")
   }
   override def mul(e1: Value, e2: Value, l: Label): Value = (e1, e2) match {
     case (TopValue, _) => TopValue
     case (_, TopValue) => TopValue
-    case (n1: NumValue, n2: NumValue) => n1.topmap(_*_, n2)
+    case (n1: NumValue, n2: NumValue) =>
+      NumValue(for (iv1 <- n1.v; iv2 <- n2.v) yield iv1 * iv2)
     case _ => throw new IllegalArgumentException(s"Expected two Interval but got ($e1, $e2)")
   }
   override def div(e1: Value, e2: Value, l: Label): Value = (e1, e2) match {
     case (TopValue, _) => TopValue
     case (_, TopValue) => TopValue
-    case (n1: NumValue, n2: NumValue) => n1.topmap(_/_, n2)
+    case (n1: NumValue, n2: NumValue) =>
+      NumValue(for (iv1 <- n1.v; iv2 <- n2.v) yield iv1 / iv2)
     case _ => throw new IllegalArgumentException(s"Expected two Interval but got ($e1, $e2)")
   }
   override def eq(e1: Value, e2: Value, l: Label): Value = (e1, e2) match {
     case (TopValue, _) => TopValue
     case (_, TopValue) => TopValue
     case (n1: NumValue, n2: NumValue) =>
-      BoolValue(for (iv1 <- n1.v; iv2 <- n2.v) yield iv1 same iv2)
-    case (b1: BoolValue, b2: BoolValue) => b1 same b2
+      BoolValue(for (iv1 <- n1.v; iv2 <- n2.v; b <- iv1 === iv2) yield b)
+    case (b1: BoolValue, b2: BoolValue) =>
+      BoolValue(for (v1 <- b1.v; v2 <- b2.v) yield v1 == v2)
     case _ => throw new IllegalArgumentException(s"Expected two values of equal type but got ($e1, $e2)")
   }
   override def lt(e1: Value, e2: Value, l: Label): Value = (e1, e2) match {
     case (TopValue, _) => TopValue
     case (_, TopValue) => TopValue
     case (n1: NumValue, n2: NumValue) =>
-      BoolValue(for (iv1 <- n1.v; iv2 <- n2.v) yield iv1.isEmpty && !iv2.isEmpty || !iv1.isEmpty && !iv2.isEmpty && iv1.h < iv2.l)
-    case (b1: BoolValue, b2: BoolValue) => b1 same b2
-    case _ => throw new IllegalArgumentException(s"Expected two values of equal type but got ($e1, $e2)")
+      BoolValue(for (iv1 <- n1.v; iv2 <- n2.v; b <- iv1 < iv2) yield b)
+    case _ => throw new IllegalArgumentException(s"Expected two Interval but got ($e1, $e2)")
   }
   override def if_[A](v: Value, thn: => A, els: => A)(implicit j: ValJoin[A]): A = v match {
-    case BoolValue(Some(true)) => thn
-    case BoolValue(Some(false)) => els
-    case BoolValue(None) => join(thn, els)
+    case BoolValue(Actual(true)) => thn
+    case BoolValue(Actual(false)) => els
+    case BoolValue(Top) => join(thn, els)
     case TopValue => join(thn, els)
     case _ => throw new IllegalArgumentException(s"Expected Bool but got $v")
   }
 }
+
+sealed trait Topped[+V] {
+  def foreach[A](f: V => A): Unit = this match {
+    case Top => // nothing
+    case Actual(v) => f(v)
+  }
+  def filter(f: V => Boolean): Topped[V] = this match {
+    case Top => Top
+    case Actual(v) => if (f(v)) this else Top
+  }
+  def withFilter(f: V => Boolean): Topped[V] = filter(f)
+  final def toString(suffix: String): String = this match {
+    case Top => s"Top$suffix"
+    case Actual(v) => v.toString
+  }
+  final def map[A](f: V => A): Topped[A] = this match {
+    case Top => Top
+    case Actual(v) => Actual(f(v))
+  }
+  final def flatMap[A](f: V => Topped[A]): Topped[A] = this match {
+    case Top => Top
+    case Actual(v) => f(v)
+  }
+}
+case object Top extends Topped[Nothing]
+case class Actual[V](v: V) extends Topped[V]
