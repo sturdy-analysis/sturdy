3810b6956489a2b61033652608cddfb25b5c534d
cheeb <cheeb@students.uni-mainz.de>
2021-09-17 11:40:39 +0200

    tests schreiben

--- sturdy-core/src/test/scala/apronIntervalsTest.scala
+++ sturdy-core/src/test/scala/apronIntervalsTest.scala
@@ -2,3 +2,15 @@
 
 import org. scalatest
 
+class ApronTest extends AnyFlatSpec, Matchers:
+  behavior of "Apron Intervals"
+
+     it must s"add to intervals" in {
+       val interval1 = ??
+       val interval2 = ??
+
+       //interval1 + interval2
+
+       //print(result)
+       assert(tree.isRight)
+     }
