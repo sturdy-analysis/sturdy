bfb00766c0c47d0a34a0a88a5af26d2f73a1f194
Katharina Brandl <brandl@uni-mainz.de>
2021-10-27 11:04:02 +0200

    test script soundness tests for constant taint analysis

---
+++ sturdy-wasm/src/test/scala/sturdy/language/wasm/analyses/constant/TestScriptConstantTaintAnalysisInterpreterTest.scala
@@ -0,0 +1,31 @@
+package sturdy.language.wasm.analyses.constant
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import sturdy.language.wasm.{TestScriptParserTest, parse, parseScript}
+
+import java.nio.file.{Files, Paths}
+import scala.io.Source
+import scala.jdk.StreamConverters.*
+
+class TestScriptConstantTaintAnalysisInterpreterTest extends AnyFlatSpec, Matchers:
+  behavior of "TestScript constant taint analysis"
+
+  val pathSpectest = Paths.get(classOf[TestScriptParserTest].getResource("/sturdy/language/wasm/spectest.wast").toURI())
+  val uri = classOf[TestScriptParserTest].getResource("/sturdy/language/wasm/scripts").toURI();
+
+  val spectest = parse(pathSpectest)
+
+  Files.list(Paths.get(uri)).toScala(List).filter(p => p.toString.endsWith(".wast") && !p.toString.endsWith("memory_grow.wast")).sorted.foreach { p =>
+    it must s"execute ${p.getFileName}" in {
+      val file = Source.fromURI(p.toUri)
+      val sourceCode = file.getLines().mkString("\n")
+      file.close()
+      val script = parseScript(sourceCode)
+      val interp = TestScriptConstantTaintAnalysisInterpreter(Some(spectest))
+      interp.run(script)
+      val interpTop = TestScriptConstantTaintAnalysisInterpreter(Some(spectest), true)
+      interpTop.run(script)
+    }
+  }
+
