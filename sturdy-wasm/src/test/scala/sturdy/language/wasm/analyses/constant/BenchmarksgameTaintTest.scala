85e9796606cc1330fb5bf449594f185f9cecf55c
Katharina Brandl <brandl@uni-mainz.de>
2021-10-29 23:41:54 +0200

    improves logging in BenchmarksgameTaintTest

--- sturdy-wasm/src/test/scala/sturdy/language/wasm/analyses/constant/BenchmarksgameTaintTest.scala
+++ sturdy-wasm/src/test/scala/sturdy/language/wasm/analyses/constant/BenchmarksgameTaintTest.scala
@@ -13,7 +13,7 @@ import sturdy.language.wasm.generic.FrameData
 import sturdy.values.Topped
 import swam.ModuleLoader
 import swam.binary.ModuleParser
-import swam.syntax.Module
+import swam.syntax.*
 import swam.validation.Validator
 
 import java.nio.file.{Files, Path, Paths}
@@ -43,9 +43,34 @@ class BenchmarksgameTaintTest extends AnyFlatSpec, Matchers:
       interp.invokeExported(modInst, funcName, List.empty)
     )
 
+    val allNodes = ControlFlow.allCfgNodes(List(modInst))
+    val allInstructions = allNodes.filter(_.isInstruction)
+    val deadInstructions = ControlFlow.deadInstruction(interp.cfg, List(modInst))
+    val deadInstructionPercent = (10000.0 * deadInstructions.size / allInstructions.size.toDouble).round / 100.0
+    println(s"Found ${deadInstructions.size} dead instructions, $deadInstructionPercent% of the ${allInstructions.size} instructions in $name")
+
+    val allLabels = allNodes.filter(_.isInstanceOf[CfgNode.Labeled])
+    val deadLabels = ControlFlow.deadLabels(interp.cfg)
+    val deadLabelsPercent = (10000.0 * deadLabels.size / allLabels.size.toDouble).round / 100.0
+    val deadLabelsGrouped = deadLabels.groupBy(_.inst.getClass.getSimpleName)
+    println(s"Found ${deadLabels.size} dead labels, $deadLabelsPercent% of the ${allLabels.size} labels in $name.")
+    val deadLabelsIf = deadLabelsGrouped.getOrElse("If", Set())
+    val deadLabelsBlock = deadLabelsGrouped.getOrElse("Block", Set())
+    val deadLabelLoop = deadLabelsGrouped.getOrElse("Loop", Set())
+    println(s"  Can optimize ${deadLabelsIf.size} if instructions; can eliminate ${deadLabelsBlock.size} block and ${deadLabelLoop.size} loop instructions.")
+
+    // TODO constant instruction logger in ConstantTaintValues
+
+    val allMemoryInstructions = allNodes.filter{
+      case CfgNode.Instruction(inst, _) => inst match
+        case _: LoadInst | _: LoadNInst | _: StoreInst | _: StoreNInst => true
+        case _ => false
+      case _ => false
+    }
     val taintedAccesses = interp.taintedMemoryAccesses.taintedMemoryAccesses
-    println(s"Found ${taintedAccesses.size} tainted memory accesses in $name.")
-    //println(interp.taintedMemoryAccesses.taintedMemoryAddresses)
+    val taintedAccessesPercent = (10000.0 * taintedAccesses.size / allMemoryInstructions.size.toDouble).round / 100.0
+    println(s"Found ${taintedAccesses.size} tainted memory accesses, $taintedAccessesPercent% of all load and store instructions in $name.")
+    println(s"  This means, ${100.0 - taintedAccessesPercent}% of all load and store instructions in $name are safe.")
 
   def readBinaryModule(path: Path): Module =
     implicit val cs = IO.contextShift(scala.concurrent.ExecutionContext.global)
