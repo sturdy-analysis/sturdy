cbc43e42e62db370f7e7a4bba8a3b0e48062851e
Sebastian Erdweg <seba.github@gmail.com>
2021-11-02 14:59:29 +0100

    trying to improve microbenchmarking, but breaking it instead

---
+++ sturdy-wasm-benchmarks/src/main/scala/sturdy/language/wasm/ConstantAnalysisState.scala
@@ -0,0 +1,16 @@
+package sturdy.language.wasm
+
+import org.openjdk.jmh.annotations.Level
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.Setup
+import org.openjdk.jmh.annotations.State
+import sturdy.language.wasm.analyses.ConstantAnalysis
+import sturdy.language.wasm.analyses.WasmConfig
+import sturdy.language.wasm.generic.FrameData
+
+@State(Scope.Thread)
+class ConstantAnalysisState:
+  var interp: ConstantAnalysis.Instance = _
+  @Setup(Level.Invocation)
+  def setup(): Unit =
+    interp = ConstantAnalysis(FrameData.empty, Iterable.empty)(WasmConfig.default)
