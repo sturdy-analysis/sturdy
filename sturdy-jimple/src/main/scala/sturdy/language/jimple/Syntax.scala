023f712c4c0cd0777b01731791fded798cacf10f
Katharina Ritter <karitter@students.uni-mainz.de>
2022-01-11 11:11:13 +0100

    Updated Syntax and added toString functions

--- sturdy-jimple/src/main/scala/sturdy/language/jimple/Syntax.scala
+++ sturdy-jimple/src/main/scala/sturdy/language/jimple/Syntax.scala
@@ -5,30 +5,50 @@ import sturdy.util.Labeled
 import com.sun.source.tree.IdentifierTree
 
 import scala.collection.Seq
+import cats.data.NonEmptyList
 
 
 type Label = Identifier
 type Identifier = String
 type SizedDims = Seq[Immediate]
-type EmptyDims = Seq[Nothing] //TODO geht das?  (empty_dims = "[]" empty_dims | ;)
-type Immediate = Constant | Local
+type EmptyDims = Int  //(empty_dims = "[]" empty_dims | ;)
+
+enum Immediate:
+  case ConstI(c: Constant)
+  case LocalI(l: Local)
+
+  override def toString: String = this match
+    case ConstI(c) => s"ConstI($c)"
+    case LocalI(l) => s"LocalI($l)"
 
 enum RVal:
-  case ArrRef(i1: Immediate, i2: Immediate)
-  case Const(c: Constant) //TODO: Ist das sinnvoll?
-  case Expression(e: Exp)
-  case InstFieldRef(i: Immediate, f: FieldSignature)
-  case Loc(l: Local)
-  case StaticFieldRef(f: FieldSignature)
+  case ArrayRefR(i1: Immediate, i2: Immediate)
+  case ConstR(c: Constant)
+  case ExpressionR(e: Exp)
+  case InstanceFieldRefR(i: Immediate, f: FieldSignature)
+  case StaticFieldRefR(f: FieldSignature)
+  case LocalR(l: Local)
 //TODO: case NextNextStmtAddr
 
+  override def toString: String = this match
+    case ArrayRefR(i1, i2) => s"ArrayRefR($i1, $i2)"
+    case ConstR(c) => s"ConstR($c)"
+    case ExpressionR(e) => s"ExpressionR($e)"
+    case InstanceFieldRefR(i, f) => s"InstanceFieldRefR($i, $f)"
+    case StaticFieldRefR(f) => s"StaticFieldRefR($f)"
+    case LocalR(l) => s"LocalR($l)"
+
 enum Var:
-  case ArrRef(i1: Immediate, i2: Immediate)
-  case InstFieldRef(i: Immediate, f: FieldSignature)
-  case StaticFieldRef(f: FieldSignature)
-  case Loc(l: Local)
+  case ArrayRefV(i1: Immediate, i2: Immediate)
+  case InstanceFieldRefV(i: Immediate, f: FieldSignature)
+  case StaticFieldRefV(f: FieldSignature)
+  case LocalV(l: Local)
 
-
+  override def toString: String = this match
+    case ArrayRefV(i1, i2) => s"ArrayRefV($i1, $i2)"
+    case InstanceFieldRefV(i, f) => s"InstanceFieldRefV($i, $f)"
+    case StaticFieldRefV(f) => s"StaticFieldRefV($f)"
+    case LocalV(l) => s"LocalV($l)"
 
 enum Stmt extends Labeled:
   case BreakpointS()
@@ -37,28 +57,62 @@ enum Stmt extends Labeled:
   case EnterMonitorS(i: Immediate)//"entermonitor" immediate
   case ExitMonitorS(i: Immediate)//"exitmonitor" immediate
   case GotoS(l: Label)//"goto" label
-  case IfS(c: Cond, l: Label)//"if" condition "then" label
-  case InvokeS(i: Exp.InvokeE)
+  case IfS(c: Exp.ConditionE, l: Label)//"if" condition "then" label
+  case InvokeS(e: Exp.InvokeE | Exp.StaticInvokeE)
   case LookupSwitchS(i: Immediate, cs: Seq[Case], l: Label)//"lookupswitch(" immediate "){" cases "default: goto" label "}"
   case NopS()
   case RetS(l: Local)//"ret" local
   case ReturnS(i: Immediate)//"ret" Immediate
   case ReturnVoidS()
-  case TableSwitchS(i: Immediate, c: Seq[Case], l: Label)//"tableswitch(" immediate "){" cases "default: goto" label "}"
+  case TableSwitchS(i: Immediate, cs: Seq[Case], l: Label)//"tableswitch(" immediate "){" cases "default: goto" label "}"
   case ThrowS(i: Immediate)//"throw" immediate
+  case LabelS(l: Label)
+
+  override def toString: String = this match
+    case BreakpointS() => s"BreakpointS()@${this.label}"
+    case AssignS(v, newVal) => s"AssignS($v, $newVal)@${this.label}"
+    case IdentityS(l, i) => s"IdentityS($l, $i)@${this.label}"
+    case EnterMonitorS(i) => s"EnterMonitorS($i)@${this.label}"
+    case ExitMonitorS(i) => s"ExitMonitorS($i)@${this.label}"
+    case GotoS(l) => s"GotoS($l)@${this.label}"
+    case IfS(c, l) => s"IfS($c, $l)@${this.label}"
+    case InvokeS(e) => s"InvokeS($e)@${this.label}"
+    case LookupSwitchS(i, cs, l) => s"LookupSwitchS($i, $cs, $l)@${this.label}"
+    case NopS() => s"NopS@${this.label}"
+    case RetS(l) => s"RetS($l)@${this.label}"
+    case ReturnS(i) => s"ReturnS($i)@${this.label}"
+    case ReturnVoidS() => s"ReturnVoidS@${this.label}"
+    case TableSwitchS(i, cs, l) => s"TableswitchS@${this.label}"
+    case ThrowS(i) => s"ThrowS@${this.label}"
+    case LabelS(l) => s"LabelS($l)@${this.label}"
+
 
 enum Exp extends Labeled:
-  case BinopE(i1: Immediate, i2: Immediate, op: BinOps | Cond)
+  case BinopE(i1: Immediate, i2: Immediate, op: BinOp | CondOp)
+  case ConditionE(i1: Immediate, i2: Immediate, op: CondOp)
   case CastE(t: Type, i: Immediate)
-  case InstOfE(i: Immediate, ref: Type.RefT)
+  case InstanceOfE(i: Immediate, ref: Type.RefT)
   case StaticInvokeE(s: MethodSignature, l: Seq[Immediate])
   case InvokeE(t: InvokeType, i: Immediate, s: MethodSignature, l: Seq[Immediate])
-  case newArrE(t: Type, i: Immediate)
-  case NewE(r: Type.RefT)
+  case NewArrayE(t: Type, i: Immediate)
+  case NewE(t: Type.RefT)
   case NewMultArrE(t: Type, dim: SizedDims, edim: EmptyDims )//"new multiarray" type sized_dims empty_dims
-  case UnopE(i: Immediate, op: Unops)
+  case UnopE(i: Immediate, op: UnOp)
 
-enum BinOps:
+  override def toString: String = this match
+    case BinopE(i1, i2, op) => s"BinOpE($i1, $i2, $op)@${this.label}"
+    case ConditionE(i1, i2, op) => s"ConditionE($i1, $i2, $op)@${this.label}"
+    case CastE(t, i) => s"CastE($t, $i)@${this.label}"
+    case InstanceOfE(i, ref) => s"InstanceOfE($i, $ref)@${this.label}"
+    case StaticInvokeE(s, l) => s"StaticInvokeE($s, $l)@${this.label}"
+    case InvokeE(t, i, s, l) => s"InvokeE($t, $i, $i, $l)@${this.label}"
+    case NewArrayE(t, i) => s"NewArrayE($t, $i)@${this.label}"
+    case NewE(t) => s"NewE($t)@${this.label}"
+    case NewMultArrE(t, dim, edim) => s"NewMultArrayE($t, $dim, $edim)@${this.label}"
+    case UnopE(i, op) => s"UnOpE($i, $op)@${this.label}"
+
+
+enum BinOp:
   case Add
   case And
   case Cmp //TODO: What are these?
@@ -74,7 +128,23 @@ enum BinOps:
   case Ushr //TODO: What are these?
   case Xor
 
-enum Cond:
+  override def toString: String = this match
+    case Add => s"Add"
+    case And => s"And"
+    case Cmp => s"Cmp"
+    case Cmpg => s"Cmpg"
+    case Cmpl => s"Cmpl"
+    case Div => s"Div"
+    case Mul => s"Mul"
+    case Or => s"Or"
+    case Rem => s"Rem"
+    case Shl => s"Shl"
+    case Shr => s"Shr"
+    case Sub => s"Sub"
+    case Ushr => s"Ushr"
+    case Xor => s"Xor"
+
+enum CondOp:
   case Eq
   case Ge
   case Le
@@ -82,49 +152,93 @@ enum Cond:
   case Gt
   case Lt
 
-enum Unops:
+  override def toString: String = this match
+    case Eq => s"Eq"
+    case Ge => s"Ge"
+    case Le => s"Le"
+    case Ne => s"Ne"
+    case Gt => s"Gt"
+    case Lt => s"Lt"
+
+enum UnOp:
   case Length(i: Immediate)
   case Neg(i: Immediate)
+  
+  override def toString: String = this match
+    case Length(i) => s"Length($i)"
+    case Neg(i) => s"Neg($i)"
 
 enum InvokeType:
   case InterfaceI
   case SpecialI
   case VirtualI
+  
+  override def toString: String = this match
+    case InterfaceI => s"InterfaceI"
+    case SpecialI => s"SpecialI"
+    case VirtualI => s"VirtualI"
 
 enum Constant:
-  case DoubleCon(v:Double)
+  case DoubleC(v:Double)
   case FloatC(v: Float)
   case IntC(v: Int)
   case LongC(v: Long)
   case StringC(v: String)
   case NullC()
+  
+  override def toString: String = this match
+    case DoubleC(v) => s"DoubleC($v)"
+    case FloatC(v) => s"FloatC($v)"
+    case IntC(v) => s"IntC($v)"
+    case LongC(v) => s"LongC($v)"
+    case StringC(v) => s"StringC($v)"
+    case NullC() => s"NullC()"
+
 
 enum IdentityVal:
   case CaughtExcRef()
-  case ParamRef(c: Constant.IntC)
-  case ThisRef()
+  case ParamRef(c: Constant.IntC, t: Type)
+  case ThisRef(t: Type)
+  
+  override def toString: String = this match
+    case CaughtExcRef() => s"CaughtExcRef()"
+    case ParamRef(c, t) => s"ParamRef($c, $t)"
+    case ThisRef(t) => s"ThisRef($t)"
 
 enum Type:
   case IntT()
   case LongT()
   case FloatT()
   case DoubleT()
-  case RefT()
-  case StmtAddT()
+  case RefT(s: String)
+  //case StmtAddressT()
   case VoidT()
+  
+  override def toString: String = this match
+    case IntT() => s"IntT()"
+    case LongT() => s"LongT()"
+    case FloatT() => s"FloatT()"
+    case DoubleT() => s"DoubleT()"
+    case RefT(s) => s"IntT($s)"
+   // case StmtAddressT() => s"StmtAddressT()"
+    case VoidT() => s"VoidT()"
 
 
-case class LocalDec(t: Type, name: String)
+
+case class LocalDec(t: Type, name: Identifier)
 
 case class Case(i: Constant.IntC, l: Label)
 
-case class MethodSignature(id: Identifier, params: Seq[Type], ret: Type)
+case class MethodSignature(id: Identifier, params: Seq[Type], ret: Type, classOrigin: String)
 
-case class FieldSignature(id: Identifier, t: Type)
+case class FieldSignature(id: Identifier, t: Type, classOrigin: String)
 
-case class ExceptionRange(ref: Type.RefT, start: Label, end: Label, cBlock: Label)
+case class ExceptionRange(ref: Type.RefT, start: Label, end: Label, catchBlock: Label)
 
-case class Method(locals: Seq[LocalDec], idStmts: Seq[Stmt.IdentityS], stmts: Seq[Stmt], excRanges: Seq[ExceptionRange])
+case class Method(header: MethodHeader, locals: Seq[LocalDec], idStmts: Seq[Stmt.IdentityS], stmts: Seq[Stmt], excRanges: Seq[ExceptionRange])
+
+case class MethodHeader(isPublic: Boolean, isPrivate: Boolean, isStatic: Boolean, ret: Type, id: Identifier, params: Seq[Type])
+case class Class(id: Identifier, extend: Option[Type.RefT], implement: Seq[Type.RefT], methods: Seq[Method])//, globals: ???)
 
 case class Program(funs: Seq[String]):
   def fold[A]: Seq[Char => ((Char, Char) => Char) => Char] =
@@ -132,7 +246,3 @@ case class Program(funs: Seq[String]):
 
 
 case class Local(id: Identifier)
-
-/*case class ArrRef(i1: Immediate, i2: Immediate)
-case class InstFieldRef(i: Immediate, f: FieldSignature)
-case class StaticFieldRef(f: FieldSignature)*/
