2d1a40c1b35398dd2157756ce776354536855593
Sebastian Erdweg <seba.github@gmail.com>
2022-07-28 14:20:50 +0200

    WIP apron integer ops

--- sturdy-apron-domain/src/main/scala/sturdy/values/integer/ApronIntegerOps.scala
+++ sturdy-apron-domain/src/main/scala/sturdy/values/integer/ApronIntegerOps.scala
@@ -1,18 +1,39 @@
 package sturdy.values.integer
 
-
-import apron.{DoubleScalar, Environment, Texpr0Node, Texpr1BinNode, Texpr1CstNode, Texpr1Node, Texpr1UnNode, Var}
+import sturdy.data.CombineUnit
+import apron.{DoubleScalar, Environment, Tcons1, Texpr0Node, Texpr1BinNode, Texpr1CstNode, Texpr1Node, Texpr1UnNode, Var}
+import sturdy.data.MayJoin.NoJoin
 import sturdy.effect.callframe.ApronCallFrame
 
 import java.util
 import math.Numeric.Implicits.infixNumericOps
 import sturdy.effect.EffectStack
+import sturdy.effect.failure.Failure
+import sturdy.values.Top
 
-given ApronIntegerOps[B, Data, Var](using Numeric[B])(using callframe: ApronCallFrame[Data, Var], effects : EffectStack) : IntegerOps[Int, Texpr1Node] with
+given ApronIntegerOps[B, Data, Var](using Numeric[B], IntegerOps[B, B], StrictIntegerOps[B, B, NoJoin], Top[NumericInterval[B]], Failure)
+                                   (using callframe: ApronCallFrame[Data, Var], effects : EffectStack) : IntegerOps[Int, Texpr1Node] with
 
-  def integerLit(i: B): Texpr1Node = new Texpr1CstNode(new DoubleScalar(i.toDouble))
+  private val intervalOps: IntervalIntegerOps[B] = StandardIntervalIntegerOps
+
+  def unaryIntervalOp(v: Texpr1Node, f: NumericInterval[B] => NumericInterval[B]): Texpr1Node =
+  // - convert both to NumericInterval
+  // - call interval operation
+  // - make into constraints about a new result apron-variable
+    ???
+  
+  def binaryIntervalOp(v1: Texpr1Node, v2: Texpr1Node, f: (NumericInterval[B], NumericInterval[B]) => NumericInterval[B]): Texpr1Node =
+  // - convert both to NumericInterval
+  // - call interval operation
+  // - make into constraints about a new result apron-variable
+    ???
+
+
+  override def integerLit(i: B): Texpr1Node = new Texpr1CstNode(new DoubleScalar(i.toDouble))
   override def integerLit(i: Int): Texpr1Node = new Texpr1CstNode(new DoubleScalar(i.toDouble))
-  override def randomInteger(): Texpr1Node = ???
+  
+  override def randomInteger(): Texpr1Node =
+    callframe.freshConstraintVariable("random")
 
   override def add(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
     new Texpr1BinNode(Texpr1BinNode.OP_ADD, v1, v2)
@@ -23,12 +44,39 @@ given ApronIntegerOps[B, Data, Var](using Numeric[B])(using callframe: ApronCall
   override def mul(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
     new Texpr1BinNode(Texpr1BinNode.OP_MUL, v1, v2)
 
-  override def max(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node = ???
-  override def min(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node = ???
+  def neg(v: Texpr1Node): Texpr1Node =
+    new Texpr1UnNode(Texpr1UnNode.OP_NEG, v)
+
+  override def max(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
+    val result = callframe.freshConstraintVariable(s"max($v1, $v2)")
+    // result >= v  iff  result - v >= 0
+    callframe.constrain(sub(result, v1), Tcons1.SUPEQ)
+    callframe.constrain(sub(result, v2), Tcons1.SUPEQ)
+    effects.joinComputations {
+      // result == v1
+      callframe.constrain(sub(result, v1), Tcons1.EQ)
+    } {
+      // result == v2
+      callframe.constrain(sub(result, v2), Tcons1.EQ)
+    }
+    result
+
+  override def min(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
+    val result = callframe.freshConstraintVariable(s"max($v1, $v2)")
+    // result <= v  iff  result - v <= 0  iff  -result + v > 0
+    callframe.constrain(add(neg(result), v1), Tcons1.SUP)
+    callframe.constrain(add(neg(result), v2), Tcons1.SUP)
+    effects.joinComputations {
+      // result == v1
+      callframe.constrain(sub(result, v1), Tcons1.EQ)
+    } {
+      // result == v2
+      callframe.constrain(sub(result, v2), Tcons1.EQ)
+    }
+    result
 
   override def div(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
     // Use join computations for call frame test
-
     new Texpr1BinNode(Texpr1BinNode.OP_DIV, v1, v2)
 
   override def divUnsigned(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node = ???
@@ -39,15 +87,28 @@ given ApronIntegerOps[B, Data, Var](using Numeric[B])(using callframe: ApronCall
   override def gcd(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node = ???
 
   override def absolute(v: Texpr1Node): Texpr1Node = ???
-  override def bitAnd(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node = ???
-  override def bitOr(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node = ???
-  override def bitXor(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node = ???
-  override def shiftLeft(v: Texpr1Node, shift: Texpr1Node): Texpr1Node = ???
-  override def shiftRight(v: Texpr1Node, shift: Texpr1Node): Texpr1Node = ???
-  override def shiftRightUnsigned(v: Texpr1Node, shift: Texpr1Node): Texpr1Node = ???
-  override def rotateLeft(v: Texpr1Node, shift: Texpr1Node): Texpr1Node = ???
-  override def rotateRight(v: Texpr1Node, shift: Texpr1Node): Texpr1Node = ???
-  override def countLeadingZeros(v: Texpr1Node): Texpr1Node = ???
-  override def countTrailingZeros(v: Texpr1Node): Texpr1Node = ???
-  override def nonzeroBitCount(v: Texpr1Node): Texpr1Node = ???
-  override def invertBits(v: Texpr1Node): Texpr1Node = ???
+
+  override def bitAnd(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v1, v2, intervalOps.bitAnd)
+  override def bitOr(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v1, v2, intervalOps.bitOr)
+  override def bitXor(v1: Texpr1Node, v2: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v1, v2, intervalOps.bitXor)
+  override def shiftLeft(v: Texpr1Node, shift: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v, shift, intervalOps.shiftLeft)
+  override def shiftRight(v: Texpr1Node, shift: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v, shift, intervalOps.shiftRight)
+  override def shiftRightUnsigned(v: Texpr1Node, shift: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v, shift, intervalOps.shiftRightUnsigned)
+  override def rotateLeft(v: Texpr1Node, shift: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v, shift, intervalOps.rotateLeft)
+  override def rotateRight(v: Texpr1Node, shift: Texpr1Node): Texpr1Node =
+    binaryIntervalOp(v, shift, intervalOps.rotateRight)
+  override def countLeadingZeros(v: Texpr1Node): Texpr1Node =
+    unaryIntervalOp(v, intervalOps.countLeadingZeros)
+  override def countTrailingZeros(v: Texpr1Node): Texpr1Node =
+    unaryIntervalOp(v, intervalOps.countTrailingZeros)
+  override def nonzeroBitCount(v: Texpr1Node): Texpr1Node =
+    unaryIntervalOp(v, intervalOps.nonzeroBitCount)
+  override def invertBits(v: Texpr1Node): Texpr1Node =
+    unaryIntervalOp(v, intervalOps.invertBits)
