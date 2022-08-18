09f891dbc09061fe221d7df65c5675b1de3b10e8
Armand Lego <armand.lego@etu.univ-nantes.fr>
2022-08-18 11:48:07 +0200

    Excluded Recursive Programs in Relational analysis

--- sturdy-tip/src/test/scala/sturdy/language/tip/analysis/RelationalAnalysisTest.scala
+++ sturdy-tip/src/test/scala/sturdy/language/tip/analysis/RelationalAnalysisTest.scala
@@ -1,6 +1,6 @@
 package sturdy.language.tip.analysis
 
-import apron.Polka
+import apron.{Polka, Texpr1Node}
 import cats.parse.{Numbers, Parser as P, Parser0 as P0}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -33,9 +33,11 @@ class RelationalAnalysisTest extends AnyFlatSpec, Matchers:
   behavior of "Tip Relational analysis"
 
   val uri = classOf[RelationalAnalysisTest].getResource("/sturdy/language/tip").toURI;
+  val recursiveProgram: Array[String] = Source.fromFile(classOf[RelationalAnalysisTest].getResource("/sturdy/language/recursive_programs").toURI).getLines.toArray
+
 
   Files.list(Paths.get(uri)).toScala(List).filter(p =>
-    p.toString.contains("") && p.toString.endsWith(".tip")
+    p.toString.contains("") && p.toString.endsWith(".tip") && !recursiveProgram.contains(p.getFileName.toString)
   ).sorted.foreach { p =>
     it must s"soundly analyze ${p.getFileName} with stacked states" in {
       runRelationalAnalysis(p, StackConfig.StackedStates())
@@ -55,25 +57,21 @@ class RelationalAnalysisTest extends AnyFlatSpec, Matchers:
     if (program.funs.exists(_.name == "main")) {
       val analysis = new RelationalAnalysis.Instance(polyManager, Map(), Map(), stackConfig, 0)
 
-      //      val onlyCalls = false
-      //      val cfg = IntervalAnalysis.controlFlow(sensitive = true, onlyCalls, analysis)
-
       val aresult = analysis.failure.fallible(analysis.execute(program))
-      Profiler.printLastMeasured()
-      LinearStateOperationCounter.addToListAndReset()
-      println(s"${LinearStateOperationCounter.toString} in the last tests")
-      println(s"#linear state operations in the last tests: ${LinearStateOperationCounter.getSummedOperationsPerTest}")
-      //      val deadNodes = cfg.filterDeadNodes(IntervalAnalysis.allCfgNodes(program, onlyCalls))
-      //      if (deadNodes.nonEmpty)
-      //        println(s"Found dead code: $deadNodes")
-    }
-/*
       val interp = ConcreteInterpreter(Map(), Map(), () => ConcreteInterpreter.Value.IntValue(0))
       val cresult = interp.failure.fallible(interp.execute(program))
+
       given CAllocationIntIncrement[AllocationSite] = interp.alloc
+
+      println(s"CONCRETE : $cresult")
+      println(s"ABSTRACT : $aresult")
+      (aresult, analysis)
+    } else {
+        null
+      }
+/*
       assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(cresult, aresult))
       assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(interp, analysis))
-      println(aresult)
       (aresult, analysis)
     } else {
       null
