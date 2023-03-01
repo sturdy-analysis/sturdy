9095a0c1aa1b888c927cb6a22ef1ce9b2acc00a5
Sebastian Erdweg <seba.github@gmail.com>
2023-03-01 11:45:52 +0100

    run binaryen script on considered files only

--- sturdy-wasm/src/test/resources/sturdy/language/wasm/wasmbench/run-binaryen.sh
+++ sturdy-wasm/src/test/resources/sturdy/language/wasm/wasmbench/run-binaryen.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 WASMOPT=/Volumes/sebahome/projects/external/binaryen/bin/wasm-opt
-TIMEOUT_SECONDS=120
 
-for f in filtered/*.wasm;
+INDIR=filtered
+
+TIMEOUT_SECONDS=60
+FILES=considered-files-${TIMEOUT_SECONDS}s.txt
+OUTDIR=binaryen-out-${TIMEOUT_SECONDS}s
+
+mkdir -p $OUTDIR
+
+# for f in filtered/*.wasm;
+while read f
 do
-  echo $WASMOPT -Oz $f -o $f.by
-  { gtimeout $TIMEOUT_SECONDS time $WASMOPT -Oz $f -o $f.by; } 2> $f.bytime
-done
+  if [ ! -f $OUTDIR/$f.bytime ]
+  then
+    echo gtimeout $TIMEOUT_SECONDS time $WASMOPT -Oz $INDIR/$f -o $OUTDIR/$f.by
+    { gtimeout $TIMEOUT_SECONDS time $WASMOPT -Oz $INDIR/$f -o $OUTDIR/$f.by; } 2> $OUTDIR/$f.bytime
+  fi
+done <$FILES
+
