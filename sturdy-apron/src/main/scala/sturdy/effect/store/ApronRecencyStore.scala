8ddb3b99a7cc35c395e36d10a3a5b822a6298471
Raphaël Monat <raphael.monat@inria.fr>
2023-10-12 18:08:49 +0200

    WIP on ApronRecencyStore

--- sturdy-apron/src/main/scala/sturdy/effect/store/ApronRecencyStore.scala
+++ sturdy-apron/src/main/scala/sturdy/effect/store/ApronRecencyStore.scala
@@ -15,59 +15,98 @@ import scala.collection.immutable.{HashMap, IntMap}
 import scala.collection.{MapView, mutable}
 import scala.reflect.ClassTag
 
-// class ApronVar[Context] extends apron.Var, sturdy.effect.store.PowPhysicalAddress[Context]:
-//   def compareTo(var : ApronVar[Context]) : int = ...
+class ApronVar[Context] extends sturdy.effect.store.PowPhysicalAddress[Context], apron.Var:
+  def compareTo(v : ApronVar[Context]) : Int = 
+    throw new NotImplementedError("ApronVar comparison")
 
+// TODO implement   (val initStore: Store[PowPhysicalAddress[Context], V, WithJoin])
+// ApronStore, then tests, then ApronCallFrame stuff
+class ApronStore[Context, V]
+  (val apronManager: Manager,  
+       getIntVal: V => Option[ApronExpr])
+  extends Store[ApronVar[Context], V, WithJoin]:
 
-class ApronRecencyStore[Context, Virt <: AbstractAddr[VirtualAddress[Context]], V]
-  (val apronManager : Manager)
-  (using initStore: Store[PowPhysicalAddress[Context], V, WithJoin])
-  (using Join[V], Widen[V], Finite[Context])
-  // missing argument for parameter initStore to massage correctly?
-  extends RecencyStore[Context, Virt, V]:
   private var apronState : Abstract1 = new Abstract1(apronManager, new Environment())
+ 
+  def read(x: ApronVar[Context]): JOptionA[V] = 
+    // FIXME: reading weak variable should be different
+    if (apronState.getEnvironment().hasVar(x)) {
+      // Which JOption here?
+      JOptionA.Some(ApronExpr.Var(x))
+    } 
+    else {
+      JOptionA.None()
+    }
 
-  override def read(vs: Virt): JOption[WithJoin, V] =
-    val pa = virtToPhys(vs)
-    // TODO: make apron.Var compatible with sturdy.effect.store.PowPhysicalAddress[Context]
-    val pa_av : apron.Var = pa
-    // Keep ApronExpr to maintain relationality? simplify?
-    makeIntVal(ApronExpr.Var(pa_av))
-
-  override def write(vs: Virt, v: V): Unit =
-    val pa  = virtToPhys(vs)
-    val pa_av : apron.Var = pa
-    val av : apron.Texpr1Intern = ApronExpr.toIntern(v)
-    val newState = apronState.assignCopy(apronManager, pa_av, av, null)
-    if (pa.isStrong)
-      apronState = newState
-    else 
-      // weak update implemented as join
-      apronState.join(apronManager, newState)
-
-  def alloc(ctx: Context): VirtualAddress[Context] =
-    val fresh = getNext()
-    mostRecent.get(ctx) match
-      case Some(mostRecentVirts) =>
-        mostRecent += ctx -> Powerset(fresh)
-        val virt = VirtualAddress(ctx, fresh, virtToPhys)
-        addressTranslation += virt.identifier -> PowRecency.Recent
-        val pa_recent_av : apron.Var = PowersetAddr(PhysicalAddress(ctx, Recency.Recent))
-        val pa_old_av : apron.Var = PowersetAddr(PhysicalAddress(ctx, Recency.Old))
-        // fold recent into old
-        apronState.fold(apronManager, Array(pa_old_av, pa_recent_av))
-        // add recent again
-        val new_env = apronState.getEnvironment().add(Array(pa_recent_av), null)
-        apronState.changeEnvironment(apronManager, new_env, false)
-        for(mostRecentVirt <- mostRecentVirts)
-          addressTranslation += (ctx,mostRecentVirt) -> PowRecency.Old
-        virt
+  def write(x: ApronVar[Context], v : V): Unit =
+    getIntVal(v) match 
+      case Some(exp) =>
+        val aexp : apron.Texpr1Intern = ApronExpr.toApron(exp)
+        val newState = apronState.assignCopy(apronManager, x, aexp, null)
+        if (x.isStrong || ! apronState.getEnvironment().hasVar(x))
+          apronState = newState
+        else 
+          // weak update implemented as join
+          apronState.join(apronManager, newState)
       case None =>
-        mostRecent += ctx -> Powerset(fresh)
-        val virt = VirtualAddress(ctx, fresh, virtToPhys)
-        addressTranslation += virt.identifier -> PowRecency.Recent
-        val pa_av : apron.Var = virtToPhys(virt)
-        // add recent
-        val new_env = apronState.getEnvironment().add(Array(pa_av), null)
-        apronState.changeEnvironment(apronManager, new_env, false)
-        virt
+        throw new NotImplementedError("")
+
+
+  def free(x: PowPhysicalAddress[Context]): Unit = throw new NotImplementedError("free")
+
+  
+
+
+// class ApronRecencyStore[Context, Virt <: AbstractAddr[VirtualAddress[Context]], V]
+//   (val apronManager : Manager)
+//   (using initStore: Store[PowPhysicalAddress[Context], V, WithJoin])
+//   (using Join[V], Widen[V], Finite[Context])
+//   // TODO: missing argument for parameter initStore to massage correctly?
+//   extends RecencyStore[Context, Virt, V]:
+//   private var apronState : Abstract1 = new Abstract1(apronManager, new Environment())
+
+//   override def read(vs: Virt): JOption[WithJoin, V] =
+//     val pa = virtToPhys(vs)
+//     // TODO: make apron.Var compatible with sturdy.effect.store.PowPhysicalAddress[Context]
+//     val pa_av : apron.Var = pa
+//     // QU: Keep ApronExpr to maintain relationality? simplify?
+//     // TODO: ApronExpr on physical address
+//     makeIntVal(ApronExpr.Var(pa_av))
+
+//   override def write(vs: Virt, v: V): Unit =
+//     val pa  = virtToPhys(vs)
+//     val pa_av : apron.Var = pa
+//     val av : apron.Texpr1Intern = ApronExpr.toIntern(v)
+//     val newState = apronState.assignCopy(apronManager, pa_av, av, null)
+//     if (pa.isStrong)
+//       apronState = newState
+//     else 
+//       // weak update implemented as join
+//       apronState.join(apronManager, newState)
+
+//   def alloc(ctx: Context): VirtualAddress[Context] =
+//     val fresh = getNext()
+//     mostRecent.get(ctx) match
+//       case Some(mostRecentVirts) =>
+//         mostRecent += ctx -> Powerset(fresh)
+//         val virt = VirtualAddress(ctx, fresh, virtToPhys)
+//         addressTranslation += virt.identifier -> PowRecency.Recent
+//         val pa_recent_av : apron.Var = PowersetAddr(PhysicalAddress(ctx, Recency.Recent))
+//         val pa_old_av : apron.Var = PowersetAddr(PhysicalAddress(ctx, Recency.Old))
+//         // fold recent into old
+//         apronState.fold(apronManager, Array(pa_old_av, pa_recent_av))
+//         // add recent again
+//         val new_env = apronState.getEnvironment().add(Array(pa_recent_av), null)
+//         apronState.changeEnvironment(apronManager, new_env, false)
+//         for(mostRecentVirt <- mostRecentVirts)
+//           addressTranslation += (ctx,mostRecentVirt) -> PowRecency.Old
+//         virt
+//       case None =>
+//         mostRecent += ctx -> Powerset(fresh)
+//         val virt = VirtualAddress(ctx, fresh, virtToPhys)
+//         addressTranslation += virt.identifier -> PowRecency.Recent
+//         val pa_av : apron.Var = virtToPhys(virt)
+//         // add recent
+//         val new_env = apronState.getEnvironment().add(Array(pa_av), null)
+//         apronState.changeEnvironment(apronManager, new_env, false)
+//         virt
