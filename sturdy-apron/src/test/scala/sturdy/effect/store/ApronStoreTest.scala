0c84d8a18dd9509e08964db7f1b0af9921b05385
Raphaël Monat <raphael.monat@inria.fr>
2023-12-12 08:44:24 +0100

    Refactor ApronStoreTest

--- sturdy-apron/src/test/scala/sturdy/effect/store/ApronStoreTest.scala
+++ sturdy-apron/src/test/scala/sturdy/effect/store/ApronStoreTest.scala
@@ -18,21 +18,38 @@ class ApronRecencyStoreTest extends AnyFunSuite:
   type PAddr = ApronPAWrap[Context]
 
   test("basic case") {
+    val man = new apron.Polka(true)
     val AS = new ApronStore[Context, ApronExpr[PAddr]](
-        new apron.Polka(true),
-        (v : ApronExpr[PAddr]) => Option(v), 
-        (e: ApronExpr[PAddr], s: Abstract1) => /* TODO: get itv overapproximation of e in s */ throw new NotImplementedError("getVal")
-    )
+          man,
+          (v : ApronExpr[PAddr]) => Option(v), 
+          (e: ApronExpr[PAddr], s: Abstract1) => ApronExpr.Constant(s.getBound(man, e.toIntern(s.getEnvironment)))
+        )
     val xR : PAddr = ApronPAWrap(PhysicalAddress("x", Recency.Recent))
     val yR : PAddr = ApronPAWrap(PhysicalAddress("y", Recency.Recent))
 
     AS.write(xR, ApronExpr.Constant(Interval(0, 10)))
-    println(xR.toString + " <- [0, 10] = " + AS.getState.toString)
+    println(s"$xR <- [0, 10] = ${AS.getState}")
 
-//
     AS.write(yR, ApronExpr.Binary(BinOp.Add, ApronExpr.Var(xR), ApronExpr.Constant(MpqScalar(1))))
-    println(yR.toString + " <- " + xR.toString + " + 1 = " + AS.getState.toString)
- 
-    
-    // AS.read(yR) ~> should just provide the corresponding interval, non-relational read shouldn't happen much
+    println(s"$yR <- $xR + 1 = ${AS.getState}")
+
+    val ry = AS.read(yR)
+    println(s"Read $yR: $ry") // non-relational read is expected here, it shouldn't happen much.
   }
+
+  // TODO: x = [0, 10], if (x >= 15)
+  // I think we need more than the ApronStore to do that?
+
+  // TODO RecencyStore+ApronStore
+  /*
+  test("with recencystore") {
+    val man = new apron.Polka(true)
+    val AS = new ApronStore[Context, ApronExpr[PAddr]](
+          man,
+          (v : ApronExpr[PAddr]) => Option(v), 
+          (e: ApronExpr[PAddr], s: Abstract1) => ApronExpr.Constant(s.getBound(man, e.toIntern(s.getEnvironment)))
+        )
+    
+  
+  }
+  */
