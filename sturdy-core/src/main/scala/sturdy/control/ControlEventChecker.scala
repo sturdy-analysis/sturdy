94440145063a5acf0aaeb6bd613716e07b29e101
Sebastian Erdweg <seba.github@gmail.com>
2024-01-29 17:43:28 +0100

    adds TestControlEvents

--- sturdy-core/src/main/scala/sturdy/control/ControlEventChecker.scala
+++ sturdy-core/src/main/scala/sturdy/control/ControlEventChecker.scala
@@ -8,6 +8,7 @@ class ControlEventChecker[Atom,Section,Exc] extends ControlObserver[Atom,Section
   private var failing = false
   private var sections: List[Section] = List()
   private var forks: List[ForkState] = List()
+  private var tries: List[Set[Exc]] = List(Set())
 
   override def handle(ev: ControlEvent[Atom, Section, Exc]): Unit =
     if (!started) ev match
@@ -31,6 +32,8 @@ class ControlEventChecker[Atom,Section,Exc] extends ControlObserver[Atom,Section
           forks = restForks
           failing = firstFailing // && failing
         case _ => error(s"Fork mismatch, expected Switch: $ev")
+      case ControlEvent.FixpointRepeat() =>
+        failing = false
       case _ => error(s"Invalid event after failure: $ev")
       
     else ev match
@@ -42,10 +45,17 @@ class ControlEventChecker[Atom,Section,Exc] extends ControlObserver[Atom,Section
         case Nil => error(s"No section to close: $ev")
         case `sec` :: restSections => sections = restSections
         case s :: _ => error(s"Section mismatch, expected End($s): $ev")
-      case ControlEvent.BeginTry() => ???
-      case ControlEvent.Throw(_) => ???
-      case ControlEvent.Catch(_) => ???
-      case ControlEvent.EndTry() => ???
+      case ControlEvent.BeginTry() => tries = Set() +: tries
+      case ControlEvent.Throw(exc) => tries = (tries.head + exc) :: tries.tail
+      case ControlEvent.Catch(exc) =>
+        if (tries.head.contains(exc))
+          tries = (tries.head - exc) :: tries.tail
+        else
+          error(s"Exception $exc not currently active")
+      case ControlEvent.EndTry() =>
+        val stillActive = tries.head
+        val rest = tries.tail
+        tries = (rest.head ++ stillActive) :: rest.tail
       case ControlEvent.Fork() => forks = ForkState.First +: forks
       case ControlEvent.Switch() => forks match
         case Nil => error(s"No fork to switch: $ev")
@@ -55,6 +65,8 @@ class ControlEventChecker[Atom,Section,Exc] extends ControlObserver[Atom,Section
         case Nil => error(s"No fork to join: $ev")
         case ForkState.Second(firstFailing) :: restForks => forks = restForks
         case _ => error(s"Fork mismatch, expected Switch: $ev")
+      case ControlEvent.FixpointAbort() => failing = true
+      case ControlEvent.FixpointRepeat() => // nothing
 
 
 object ControlEventChecker:
@@ -64,3 +76,4 @@ object ControlEventChecker:
   private enum ForkState:
     case First
     case Second(firstFailing: Boolean)
+
--- sturdy-core/src/test/scala/sturdy/control/TestControlEvents.scala
+++ sturdy-core/src/test/scala/sturdy/control/TestControlEvents.scala
@@ -1,208 +1,222 @@
 package sturdy.control
 
 import org.scalatest.funsuite.AnyFunSuite
+import ControlEvent.*
 
 class TestControlEvents extends AnyFunSuite {
-  /**
-  * x = a
-  * while (x)
-  *   x = x - 1
-  * y = x
-  */
-  import ControlEvent.*
-  val es = List(
-    Start(),
-    Atomic("x = a"),
-    Atomic("while (x)"),
-    Atomic("x = x - 1"),
-    Atomic("while (x)"),
-    FixpointAbort(),
-    FixpointRepeat(),
-    Atomic("while (x)"),
-    Atomic("x = x - 1"),
-    Atomic("while (x)"),
-    Atomic("y = x")
+  inline def testEvents[A,S,E](name: String, code: String, es: List[ControlEvent[A,S,E]]): Unit =
+    test(name) {
+      val rec = new RecordingControlObserver[A, S, E](true)
+      es.foreach(rec.handle)
+      rec.ch
+    }
+
+  testEvents("Test 1",
+    s"""
+     |x = a
+     |while (x)
+     |  x = x - 1
+     |y = x
+     |""".stripMargin,
+    List(
+      Start(),
+      Atomic("x = a"),
+      Atomic("while (x)"),
+      Atomic("x = x - 1"),
+      Atomic("while (x)"),
+      FixpointAbort(),
+      FixpointRepeat(),
+      Atomic("while (x)"),
+      Atomic("x = x - 1"),
+      Atomic("while (x)"),
+      Atomic("y = x")
+    )
   )
 
-  /**
-    * def f(n)=
-    *   var x;
-    *   if (n <= 0)
-    *     x = 1
-    *   else
-    *     x = n * f(n - 1)
-    *   ret x
-    */
-  val es2 = List(
-    Begin("f"),
-    Atomic("if"),
-    Fork(),
-    Atomic("x = 1"),
-    Switch(),
-    FixpointAbort(),
-    Join(),
-    Atomic("ret x"),
-    End("f"),
-
-    FixpointRepeat(),
-    Begin("f"),
-    Atomic("if"),
-    Fork(),
-    Atomic("x = 1"),
-    Switch(),
-    Atomic("x = n * f(n-1)"),
-    Join(),
-    Atomic("ret x"),
-    End("f"),
-  )
-
-
-  /** def f() =
-    *   try
-    *     x = 1
-    *     throw A
-    *     x = 2
-    *   catch A =>
-    *     x = 3
-    *   catch B =>
-    *     x = 4
-    *   ret x
-    */
-  val es3 = List(
-    Begin("f"),
-    BeginTry(),
+  testEvents("Test 2",
+    s"""  var x;
+       |  if (n <= 0)
+       |    x = 1
+       |  else
+       |    x = n * f(n - 1)
+       |  ret x""".stripMargin,
+    List(
+      Start(),
+      Begin("f"),
+      Atomic("if"),
+      Fork(),
       Atomic("x = 1"),
-      Throw("A"),
-      Catch("A"),
-      Atomic("x = 3"),
-    EndTry(),
-    End("f")
-  )
+      Switch(),
+      FixpointAbort(),
+      Join(),
+      Atomic("ret x"),
+      End("f"),
 
-  /** def f() =
-    *   try
-    *     x = 1
-    *     throw B
-    *     x = 2
-    *   catch A =>
-    *     x = 3
-    *   catch B =>
-    *     x = 4
-    *   ret x
-    */
-  val es4 = List(
-    Begin("f"),
-    BeginTry(),
+      FixpointRepeat(),
+      Begin("f"),
+      Atomic("if"),
+      Fork(),
       Atomic("x = 1"),
-      Throw("B"),
-      Catch("B"),
-      Atomic("x = 4"),
-    EndTry(),
-    End("f")
+      Switch(),
+      Atomic("x = n * f(n-1)"),
+      Join(),
+      Atomic("ret x"),
+      End("f"),
+    )
   )
 
-  /** def f() =
-    *   try
-    *     If (???) {
-    *       x = 1; throw A
-    *     } else {
-    *       x = 2
-    *     }
-    *     x = 3
-    *   catch A =>
-    *     x = 4
-    *   ret x
-    */
-  val es5 = List(
-    Begin("f"),
-    BeginTry(),
-      Atomic("If(???)"),
-      Fork(),
-        Atomic("x = 1"),
-        Throw("A"),
-      Switch(),
-        Atomic("x = 2"),
-      Join(),
-      Atomic("x = 3"),
-      Fork(),
-      Switch(),
-        Catch("A"),
-        Atomic("x = 4"),
-      Join(),
-    EndTry(),
-    End("f")
-  )
 
-  /** def f() =
-    *   try
-    *     If (???) {
-    *       x = 1; throw A
-    *     } else {
-    *       x = 2; throw B
-    *     }
-    *   catch A =>
-    *     x = 3
-    *   catch B =>
-    *     x = 4
-    *   ret x
-    */
-  val es6 = List(
-    Begin("f"),
-    BeginTry(),
-      Atomic("If(???)"),
-      Fork(),
-        Atomic("x = 1"),
-        Throw("A"),
-      Switch(),
-        Atomic("x = 2"),
-        Throw("B"),
-      Join(),
-      Fork(),
-        Catch("A"),
-        Atomic("x = 3"),
-      Switch(),
-        Catch("B"),
-        Atomic("x = 4"),
-      Join(),
-    EndTry(),
-    End("f")
-  )
-
-  /** def f() =
-    *   try
-    *     If (???) {
-    *       x = 1; throw A
-    *     } else {
-    *       x = 2
-    *     }
-    *     try
-    *       x = 3
-    *     catch A =>
-    *       x = -1
-    *   catch A =>
-    *     x = 4
-    *   ret x
-    */
-  val es7 = List(
-    Begin("f"),
-    BeginTry(),
-      Atomic("If(???)"),
-      Fork(),
-        Atomic("x = 1"),
-        Throw("A"),
-      Switch(),
-        Atomic("x = 2"),
-      Join(),
+  testEvents("Test 3",
+    s"""  try
+       |    x = 1
+       |    throw A
+       |    x = 2
+       |  catch A =>
+       |    x = 3
+       |  catch B =>
+       |    x = 4
+       |  ret x""".stripMargin,
+    List(
+      Start(),
+      Begin("f"),
       BeginTry(),
+        Atomic("x = 1"),
+        Throw("A"),
+        Catch("A"),
         Atomic("x = 3"),
       EndTry(),
-      Fork(),
-      Switch(),
-        Catch("A"),
+      End("f")
+    )
+  )
+
+  testEvents("Test 4",
+    s"""  try
+       |    x = 1
+       |    throw B
+       |    x = 2
+       |  catch A =>
+       |    x = 3
+       |  catch B =>
+       |    x = 4
+       |  ret x""".stripMargin,
+    List(
+      Start(),
+      Begin("f"),
+      BeginTry(),
+        Atomic("x = 1"),
+        Throw("B"),
+        Catch("B"),
         Atomic("x = 4"),
-      Join(),
-    EndTry(),
-    End("f")
+      EndTry(),
+      End("f")
+    )
+  )
+
+  testEvents("Test 5",
+    s"""  try
+       |    If (???) {
+       |      x = 1; throw A
+       |    } else {
+       |      x = 2
+       |    }
+       |    x = 3
+       |  catch A =>
+       |    x = 4
+       |  ret x""".stripMargin,
+    List(
+      Start(),
+      Begin("f"),
+      BeginTry(),
+        Atomic("If(???)"),
+        Fork(),
+          Atomic("x = 1"),
+          Throw("A"),
+        Switch(),
+          Atomic("x = 2"),
+        Join(),
+        Atomic("x = 3"),
+        Fork(),
+        Switch(),
+          Catch("A"),
+          Atomic("x = 4"),
+        Join(),
+      EndTry(),
+      End("f")
+    )
+  )
+
+  testEvents("Test 6",
+    s"""  try
+       |    If (???) {
+       |      x = 1; throw A
+       |    } else {
+       |      x = 2; throw B
+       |    }
+       |  catch A =>
+       |    x = 3
+       |  catch B =>
+       |    x = 4
+       |  ret x""".stripMargin,
+    List(
+      Start(),
+      Begin("f"),
+      BeginTry(),
+        Atomic("If(???)"),
+        Fork(),
+          Atomic("x = 1"),
+          Throw("A"),
+        Switch(),
+          Atomic("x = 2"),
+          Throw("B"),
+        Join(),
+        Fork(),
+          Catch("A"),
+          Atomic("x = 3"),
+        Switch(),
+          Catch("B"),
+          Atomic("x = 4"),
+        Join(),
+      EndTry(),
+      End("f")
+    )
+  )
+
+  testEvents("Test 7",
+    s"""  try
+       |    If (???) {
+       |      x = 1; throw A
+       |    } else {
+       |      x = 2
+       |    }
+       |    try
+       |      x = 3
+       |    catch A =>
+       |      x = -1
+       |  catch A =>
+       |    x = 4
+       |  ret x""".stripMargin,
+    List(
+      Start(),
+      Begin("f"),
+      BeginTry(),
+        Atomic("If(???)"),
+        Fork(),
+          Atomic("x = 1"),
+          Throw("A"),
+        Switch(),
+          Atomic("x = 2"),
+        Join(),
+        BeginTry(),
+          Atomic("x = 3"),
+        EndTry(),
+        Fork(),
+        Switch(),
+          Catch("A"),
+          Atomic("x = 4"),
+        Join(),
+      EndTry(),
+      End("f")
+    )
   )
 
 }
