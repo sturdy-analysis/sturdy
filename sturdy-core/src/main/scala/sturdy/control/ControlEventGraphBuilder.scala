be30d0fe73664adef911c7a1aeacd151c06c97a0
Armand Lego <alego@uni-mainz.de>
2024-01-31 17:03:45 +0100

    Added unit tests

--- sturdy-core/src/main/scala/sturdy/control/ControlEventGraphBuilder.scala
+++ sturdy-core/src/main/scala/sturdy/control/ControlEventGraphBuilder.scala
@@ -16,15 +16,9 @@ enum Node:
 enum EdgeType:
   case CF
   case BlockPair
-  /*
-  case Thn
-  case Els
-  case JoinThn
-  case JoinEls
-  case EmptyEls
-   */
 
-type ProtoEdge = (Node, EdgeType)
+
+type IncompleteEdge = (Node, EdgeType)
 
 type Edge = (Node, Node, EdgeType)
 
@@ -32,7 +26,7 @@ class ControlEventGraphBuilder[Atom,Section,Exc] extends ControlObserver[Atom,Se
   import Node.*
   import EdgeType.*
 
-  var stack: mutable.Stack[List[ProtoEdge]] = mutable.Stack.empty
+  var stack: mutable.Stack[List[IncompleteEdge]] = mutable.Stack.empty
   var edges: mutable.Set[Edge] = mutable.Set.empty
 
   var started: Boolean = false
@@ -47,7 +41,7 @@ class ControlEventGraphBuilder[Atom,Section,Exc] extends ControlObserver[Atom,Se
     else
       ev match
         case ControlEvent.Start() => throw new Exception("Duplicate start")
-        
+
         case ControlEvent.Atomic(a: Atom) => // POP 1 PUSH 1
           val current = Node.Atomic(a.toString)
           completeEdge(current)
@@ -60,7 +54,7 @@ class ControlEventGraphBuilder[Atom,Section,Exc] extends ControlObserver[Atom,Se
 
           stack.push(List((current, BlockPair)))
           stack.push(List((current, CF)))
-          
+
         case ControlEvent.End(sec: Section) => // POP 2 PUSH 1
           val current = Node.BlockEnd(sec.toString)
           completeEdge(current)
@@ -74,11 +68,6 @@ class ControlEventGraphBuilder[Atom,Section,Exc] extends ControlObserver[Atom,Se
           stack.push(prev)
           stack.push(prev)
 
-          /*
-          stack.push(prev.map((n, _) => (n, Els)))
-          stack.push(prev.map((n, _) => (n, Thn)))
-           */
-
         case ControlEvent.Switch() => // POP 2 PUSH 2 (swap top 2)
           val lastThn = stack.pop
           val elsEdge = stack.pop
@@ -90,7 +79,7 @@ class ControlEventGraphBuilder[Atom,Section,Exc] extends ControlObserver[Atom,Se
           val lastEls = stack.pop
           val lastThn = stack.pop
 
-          stack.push(lastEls/*.map((n, et) => (n, if et == Els then CF else JoinEls))*/ ++ lastThn/*.map((n, _) => (n, JoinThn))*/)
+          stack.push(lastEls ++ lastThn)
 
         case ControlEvent.Failed() =>
           val current = Node.Failed("")
@@ -102,9 +91,9 @@ class ControlEventGraphBuilder[Atom,Section,Exc] extends ControlObserver[Atom,Se
   private def completeEdge(current : Node) : Unit = completeEdge(current, None)
 
   private def completeEdge(current : Node, edgeType: Option[EdgeType]) : Unit =
-    val protoEdges : List[ProtoEdge] = stack.pop()
-    protoEdges.foreach {
-      (prev, et : EdgeType) => edges += ((prev, current, edgeType.getOrElse(et) ))
+    val protoEdges : List[IncompleteEdge] = stack.pop()
+    protoEdges.foreach { (prev, et : EdgeType) =>
+      edges += ((prev, current, edgeType.getOrElse(et) ))
     }
 
   def toGraphViz : String =
@@ -112,11 +101,4 @@ class ControlEventGraphBuilder[Atom,Section,Exc] extends ControlObserver[Atom,Se
     edges.map {
       case ((n1, n2, CF)) => s"\"${n1}\" -> \"${n2}\""
       case ((n1, n2, BlockPair)) => s"\"${n1}\" -> \"${n2}\"  [style=dashed]"
-      /*
-      case ((n1, n2, Thn)) => s"\"${n1}\" -> \"${n2}\"  [label=\"then\" color=green]"
-      case ((n1, n2, Els)) => s"\"${n1}\" -> \"${n2}\"  [label=\"else\" color=blue]"
-      case ((n1, n2, JoinThn)) => s"\"${n1}\" -> \"${n2}\"  [label=\"JoinThen\" color=green]"
-      case ((n1, n2, JoinEls)) => s"\"${n1}\" -> \"${n2}\"  [label=\"JoinElse\" color=blue]"
-      case ((n1, n2, EmptyEls)) => s"\"${n1}\" -> \"${n2}\"  [label=\"EmptyEls\" color=yellow]"
-      */
     }.reduce((s1, s2) => s1 + "\n" + s2)
---
+++ sturdy-core/src/test/scala/sturdy/control/TestGraphBuilder.scala
@@ -0,0 +1,232 @@
+package sturdy.control
+
+import org.scalatest.funsuite.AnyFunSuite
+import ControlEvent.*
+
+
+class TestGraphBuilder extends AnyFunSuite {
+
+  inline def testGraph[A, S, E](name: String, code: String, es: List[ControlEvent[A, S, E]], expected: Set[(Node, Node, EdgeType)]): Unit =
+    test(name) {
+      val graphBuilder = new ControlEventGraphBuilder[A, S, E]
+      es.foreach(graphBuilder.handle)
+
+      val edgesMissing = expected.diff(graphBuilder.edges)
+      val edgesUnexpected = graphBuilder.edges.diff(expected)
+
+      (edgesMissing.isEmpty, edgesUnexpected.isEmpty) match
+        case (true, true) => ()
+        case (false, true) => throw new Exception(s"Missing expected edges : ${edgesMissing}")
+        case (true, false) => throw new Exception(s"Unexpected edges : ${edgesUnexpected}")
+        case (false, false) => throw new Exception(s"Missing and unexpected edges\n\tMissing edges : ${edgesMissing}\n\tUnexpected edges : ${edgesUnexpected}")
+    }
+    ()
+
+  private def linearPath(nodes: Seq[Node]): Set[(Node, Node, EdgeType)] =
+    nodes.sliding(2).map {
+      case Seq(a, b) => ((a, b, EdgeType.CF))
+    }.toSet
+
+
+  testGraph("Atoms",
+    s"""
+       |var x,y,z;
+       |x = input;
+       |y = x + 1;
+       |z = input;
+       |output z;
+       |""".stripMargin,
+    List(
+      Start(),
+      Atomic("x = input"),
+      Atomic("y = x + 1"),
+      Atomic("z = input"),
+      Atomic("output z"),
+    ),
+    linearPath(List(Node.Start(),
+      Node.Atomic("x = input"),
+      Node.Atomic("y = x + 1"),
+      Node.Atomic("z = input"),
+      Node.Atomic("output z")))
+  )
+
+  testGraph("Blocks",
+    s"""
+       |main() {
+       |  output 3;
+       |  return 0;
+       |}
+       |""".stripMargin,
+    List(
+      Start(),
+      Begin("Call(main)"),
+      Begin("main"),
+      Atomic("output 3"),
+      End("main"),
+      End("Call(main)"),
+    ),
+    linearPath(List(Node.Start(),
+      Node.BlockStart("Call(main)"),
+      Node.BlockStart("main"),
+      Node.Atomic("output 3"),
+      Node.BlockEnd("main"),
+      Node.BlockEnd("Call(main)")))
+      ++ Set(
+      (Node.BlockStart("main"), Node.BlockEnd("main"), EdgeType.BlockPair),
+      (Node.BlockStart("Call(main)"), Node.BlockEnd("Call(main)"), EdgeType.BlockPair),
+    )
+  )
+
+  testGraph("Nested blocks (call)",
+    s"""
+       |main() {
+       |  var x;
+       |  output 3;
+       |  x = foo()
+       |  output x;
+       |  return 0;
+       |}
+       |
+       |foo() {
+       |  return 5;
+       |}
+       |""".stripMargin,
+    List(
+      Start(),
+      Begin("Call(main)"),
+      Begin("main"),
+      Atomic("output 3"),
+      Begin("Call(foo)"),
+      Begin("foo"),
+      End("foo"),
+      End("Call(foo)"),
+      Atomic("x = foo()"),
+      Atomic("output x"),
+      End("main"),
+      End("Call(main)"),
+    ),
+    linearPath(List(Node.Start(),
+      Node.BlockStart("Call(main)"),
+      Node.BlockStart("main"),
+      Node.Atomic("output 3"),
+      Node.BlockStart("Call(foo)"),
+      Node.BlockStart("foo"),
+      Node.BlockEnd("foo"),
+      Node.BlockEnd("Call(foo)"),
+      Node.Atomic("x = foo()"),
+      Node.Atomic("output x"),
+      Node.BlockEnd("main"),
+      Node.BlockEnd("Call(main)")))
+      ++ Set(
+      (Node.BlockStart("main"), Node.BlockEnd("main"), EdgeType.BlockPair),
+      (Node.BlockStart("Call(main)"), Node.BlockEnd("Call(main)"), EdgeType.BlockPair),
+      (Node.BlockStart("foo"), Node.BlockEnd("foo"), EdgeType.BlockPair),
+      (Node.BlockStart("Call(foo)"), Node.BlockEnd("Call(foo)"), EdgeType.BlockPair),
+    )
+  )
+
+  testGraph("Fork",
+    s"""
+       |main() {
+       |  val x;
+       |  x = input;
+       |  if(x) {
+       |    output 0;
+       |  }
+       |  else {
+       |    output 1;
+       |  }
+       |  return 0;
+       |}
+       |""".stripMargin,
+    List(
+      Start(),
+      Begin("Call(main)"),
+      Begin("main"),
+      Atomic("x = input"),
+      Atomic("if(x)"),
+      Fork(),
+      Atomic("output 0"),
+      Switch(),
+      Atomic("output 1"),
+      Join(),
+      End("main"),
+      End("Call(main)"),
+    ),
+    linearPath(List(
+      Node.Start(),
+      Node.BlockStart("Call(main)"),
+      Node.BlockStart("main"),
+      Node.Atomic("x = input"),
+      Node.Atomic("if(x)"),
+      Node.Atomic("output 0"),
+      Node.BlockEnd("main"),
+      Node.BlockEnd("Call(main)")
+    ))
+      ++ Set(
+      (Node.Atomic("if(x)"), Node.Atomic("output 1"), EdgeType.CF), // Second part of the Fork
+      (Node.Atomic("output 1"), Node.BlockEnd("main"), EdgeType.CF), // Second part of the Fork
+      (Node.BlockStart("main"), Node.BlockEnd("main"), EdgeType.BlockPair),
+      (Node.BlockStart("Call(main)"), Node.BlockEnd("Call(main)"), EdgeType.BlockPair),
+    )
+  )
+
+  testGraph("Nested fork with empty then",
+    s"""
+       |main() {
+       |  val x;
+       |  x = input;
+       |  if(x) {
+       |    output 0;
+       |    if(x>10) {
+       |      output 2;
+       |    }
+       |  }
+       |  else {
+       |    output 1;
+       |  }
+       |  return 0;
+       |}
+       |""".stripMargin,
+    List(
+      Start(),
+      Begin("Call(main)"),
+      Begin("main"),
+      Atomic("x = input"),
+      Atomic("if(x)"),
+      Fork(),
+      Atomic("output 0"),
+      Atomic("if(x>10)"),
+      Fork(),
+      Atomic("output 2"),
+      Switch(),
+      Join(),
+      Switch(),
+      Atomic("output 1"),
+      Join(),
+      End("main"),
+      End("Call(main)"),
+    ),
+    linearPath(List(
+      Node.Start(),
+      Node.BlockStart("Call(main)"),
+      Node.BlockStart("main"),
+      Node.Atomic("x = input"),
+      Node.Atomic("if(x)"),
+      Node.Atomic("output 0"),
+      Node.Atomic("if(x>10)"),
+      Node.Atomic("output 2"),
+      Node.BlockEnd("main"),
+      Node.BlockEnd("Call(main)")
+    ))
+      ++ Set(
+      (Node.Atomic("if(x)"), Node.Atomic("output 1"), EdgeType.CF), // Second part of the Fork
+      (Node.Atomic("output 1"), Node.BlockEnd("main"), EdgeType.CF), // Second part of the Fork
+      (Node.Atomic("if(x>10)"), Node.BlockEnd("main"), EdgeType.CF), // Second part of the nested Fork
+      (Node.BlockStart("main"), Node.BlockEnd("main"), EdgeType.BlockPair),
+      (Node.BlockStart("Call(main)"), Node.BlockEnd("Call(main)"), EdgeType.BlockPair),
+    )
+  )
+
+}
+
