0565dacc103bed2f854312b014e42cdf95be61b0
Armand Lego <alego@uni-mainz.de>
2024-02-09 11:15:51 +0100

    Simplification of Failure handling for ControlTreeGraphBuilder

--- sturdy-core/src/main/scala/sturdy/control/ControlTreeGraphBuilder.scala
+++ sturdy-core/src/main/scala/sturdy/control/ControlTreeGraphBuilder.scala
@@ -14,58 +14,61 @@ class ControlTreeGraphBuilder[Atom, Sec, Exc] {
     rec(ct, Set.empty)
     edges
 
-  private def rec(ct: CT, prev: Set[CNode]): (Set[CNode], Map[Exc, Set[CNode]], Boolean) = ct match
+  private def rec(ct: CT, prev: Set[CNode]): (Set[CNode], Map[Exc, Set[CNode]]) = ct match
     case ControlTree.Empty() =>
-      (prev, Map.empty, false)
+      (prev, Map.empty)
 
     case ControlTree.Atomic(a) =>
       val node: CNode = Node.Atomic(a)
       addEdges(prev, node)
-      (Set(node), Map.empty, false)
+      (Set(node), Map.empty)
 
     case ControlTree.Seq(x, xs) =>
-      val (prevAfter1, exc1, failing1) = rec(x, prev)
-      val (prevAfter2, exc2, failing2) = rec(xs, prevAfter1)
-      (prevAfter2, combineMaps(exc1, exc2, _ ++ _), failing1 || failing2)
+      val (prevAfter1, exc1) = rec(x, prev)
+      assert(prevAfter1.nonEmpty)
+      val (prevAfter2, exc2) = rec(xs, prevAfter1)
+      (prevAfter2, combineMaps(exc1, exc2, _ ++ _))
 
     case ControlTree.Section(section, body) =>
       val nodeStart: CNode = Node.BlockStart(section)
       val nodeEnd: CNode = Node.BlockEnd(section)
-      val (prevEndBlock, excBlock, failing) = rec(body, Set(nodeStart))
+      val (prevEndBlock, excBlock) = rec(body, Set(nodeStart))
       addEdges(prev, nodeStart)
-      if (!failing)
+      if (prevEndBlock.nonEmpty)
         addEdges(prevEndBlock, nodeEnd)
-      if (!edges.contains(Edge(nodeStart, nodeEnd, EdgeType.CF)) && edges.exists(e => e.to == nodeEnd))
-        edges += Edge(nodeStart, nodeEnd, EdgeType.BlockPair)
-      (Set(nodeEnd), excBlock, failing)
+        if (!edges.contains(Edge(nodeStart, nodeEnd, EdgeType.CF)) && edges.exists(e => e.to == nodeEnd))
+          edges += Edge(nodeStart, nodeEnd, EdgeType.BlockPair)
+        (Set(nodeEnd), excBlock)
+      else
+        (Set.empty, excBlock)
 
     case ControlTree.Fork(b1, b2) =>
-      val (lastBlock1, exc1, failing1) = rec(b1, prev)
-      val (lastBlock2, exc2, failing2) = rec(b2, prev)
-      (lastBlock1 ++ lastBlock2, combineMaps(exc1, exc2, _ ++ _), failing1 && failing2)
+      val (lastBlock1, exc1) = rec(b1, prev)
+      val (lastBlock2, exc2) = rec(b2, prev)
+      (lastBlock1 ++ lastBlock2, combineMaps(exc1, exc2, _ ++ _))
 
     case ControlTree.Failed() =>
       val current: CNode = Node.Failure()
       addEdges(prev, current)
-      (Set.empty, Map.empty, true)
+      (Set.empty, Map.empty)
 
     case ControlTree.Throw(exc) =>
-      (Set.empty, Map(exc -> prev), false)
+      (Set.empty, Map(exc -> prev))
 
     case ControlTree.Try(body, handlers) =>
-      val (endTry, throwers, bodyFailing) = rec(body, prev)
+      val (endTry, throwers) = rec(body, prev)
 
-      val (lastHandlers, excHandlers, failing) = throwers.map((exc, thrower) => handlers.get(exc) match
+      val (lastHandlers, excHandlers) = throwers.map((exc, thrower) => handlers.get(exc) match
         case Some(handle) => rec(handle, thrower)
-        case None => (Set.empty, Map(exc -> thrower), false)
-      ).unzip3
-      (lastHandlers.flatten.toSet ++ endTry, excHandlers.fold(Map.empty)(combineMaps(_, _, _ ++ _)), failing.fold(bodyFailing)(_ || _))
+        case None => (Set.empty, Map(exc -> thrower))
+      ).unzip
+      (lastHandlers.flatten.toSet ++ endTry, excHandlers.fold(Map.empty)(combineMaps(_, _, _ ++ _)))
 
     case ControlTree.Fixpoint(b, rep) =>
-      val (endBody, excBody, failing) = rec(b, prev)
+      val (endBody, excBody) = rec(b, prev)
       rep match
-        case None => (endBody, excBody, failing)
-        case Some(next) => rec(next, prev) // failing product ?
+        case None => (endBody, excBody)
+        case Some(next) => rec(next, prev)
 
   private inline def addEdges(prev: Set[CNode], current: CNode): Unit =
     edges ++= prev.map(p => Edge(p, current, EdgeType.CF))
