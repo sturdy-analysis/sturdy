7598e56da42edbc7821abb00e23c0f91626aee0e
Sven Keidel <svenkeidel@pm.me>
2024-02-16 09:43:15 +0100

    add special case join for apron expressions inside the apron store

--- sturdy-apron/src/main/scala/sturdy/effect/store/ApronRecencyStore.scala
+++ sturdy-apron/src/main/scala/sturdy/effect/store/ApronRecencyStore.scala
@@ -5,7 +5,7 @@ import sturdy.apron.{ApronExpr, ApronRecencyState, ApronState, ApronType, given}
 import sturdy.effect.Stateless
 import sturdy.effect.allocation.Allocator
 import sturdy.effect.store.{ApronStore, RecencyStore, given}
-import sturdy.values.{Finite, Join, Widen}
+import sturdy.values.{Finite, Join, MaybeChanged, Widen}
 import sturdy.values.references.{*, given}
 
 import scala.reflect.ClassTag
@@ -20,6 +20,16 @@ object ApronRecencyStore:
     type ApronExprVirtAddr = ApronExpr[VirtualAddress[Ctx], Type]
     type ApronExprPhysAddr = ApronExpr[PhysicalAddress[Ctx], Type]
 
+    // Special case join on apron expression used only in ApronStore.
+    // We can assume that we only join on constant apron expressions.
+    given Join[ApronExprPhysAddr] = {
+      case (ApronExpr.Constant(coeff1,tpe1), ApronExpr.Constant(coeff2, tpe2)) =>
+        val joinedCoeff = Join(coeff1, coeff2)
+        val joinedTypes = Join(tpe1, tpe2)
+        MaybeChanged(ApronExpr.Constant(joinedCoeff.get, joinedTypes.get), joinedCoeff.hasChanged || joinedTypes.hasChanged)
+      case (e1,e2) => throw IllegalArgumentException(s"Unexpected apron expression as arguments to join ${(e1,e2)}")
+    }
+  
     val apronStore: ApronStore[Ctx, Type, PowPhysAddr, ApronExprPhysAddr] = ApronStore(
       apronManager,
       Abstract1(apronManager, new Environment()),
