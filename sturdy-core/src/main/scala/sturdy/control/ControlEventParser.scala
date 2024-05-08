f03ff99ff82ce9608ea8c2c85c5e3b388408279c
Armand Lego <alego@uni-mainz.de>
2024-05-08 11:45:27 +0200

    Parser with new stack

--- sturdy-core/src/main/scala/sturdy/control/ControlEventParser.scala
+++ sturdy-core/src/main/scala/sturdy/control/ControlEventParser.scala
@@ -34,17 +34,33 @@ class ControlEventParser[Atom,Section,Exc,Fx] extends ControlObserver[Atom,Secti
           stack = addTree(CT.Section(sec, t), stack_)
         case _ => error(s"Entry mismatch, expected end of $ev: $stack")
 
-  private def addTree(t: CT, st: List[Entry]): List[Entry] = st match
-    case Entry.BaseTree(t0) :: st_ => Entry.BaseTree(CT.Seq(t0, t)) :: st_
-    case Entry.Sec(s, t0) :: st_ => Entry.Sec(s, CT.Seq(t0, t)) :: st_
-    case Entry.Try(t0) :: st_ => Entry.Try(CT.Seq(t0, t)) :: st_
-    case Entry.Handler(exc, t0) :: st_ => Entry.Handler(exc, CT.Seq(t0, t)) :: st_
-    case Entry.ForkFirst(t0) :: st_ => Entry.ForkFirst(CT.Seq(t0, t)) :: st_
-    case Entry.ForkSecond(first, t0) :: st_ => Entry.ForkSecond(first, CT.Seq(t0, t)) :: st_
-    case Entry.Fixpoint(fx, t0) :: st_ => Entry.Fixpoint(fx, CT.Seq(t0, t)) :: st_
-    case Entry.Catching(_, _) :: st_ => error(s"Ill-formatted stack, top entry must not be Catching : $stack")
-    case Nil => error(s"Stack underflow: cannot add tree $t")
-    case _ => assert(false)
+  override def handle(ev: ExceptionControlEvent[Atom,Section,Exc,Fx]): Unit =
+    import ExceptionControlEvent.*
+    ev match
+      case BeginTry() =>
+        assertNoCatching()
+        stack = Entry.Try(CT.Empty()) :: stack
+      case Throw(exc: Exc) =>
+        assertNoCatching()
+        addTree(CT.Throw(exc))
+      case Catching() => stack match
+        case Entry.Try(t) :: stack_ =>
+          stack = Entry.Catching(t, List()) :: stack_
+        case _ => error(s"Entry mismatch, expected Try for $ev: $stack")
+      case BeginHandle(exc: Exc) => stack match
+        case Entry.Catching(body, hs) :: stack_ =>
+          stack = Entry.Handler(exc, CT.Empty(), body, hs) :: stack_
+        case _ => error(s"Entry mismatch, expected Catching for $ev: $stack")
+      case EndHandle() => stack match
+        case Entry.Handler(hx, t, body, hs) :: stack_ =>
+          stack = Entry.Catching(body, hs :+ (hx, t)) :: stack_
+        case _ => error(s"Entry mismatch, expected Handler for $ev: $stack")
+      case EndTry() => stack match
+        case Entry.Try(t) :: stack_ =>
+          stack = addTree(CT.Try(t, Nil), stack_)
+        case Entry.Catching(body, hs) :: stack_ =>
+          stack = addTree(CT.Try(body, hs), stack_)
+        case _ => error(s"Entry mismatch, expected Try or Catching for $ev: $stack")
 
   def assertNoCatching(): Unit =
     if (isCatching) {
@@ -69,33 +85,17 @@ class ControlEventParser[Atom,Section,Exc,Fx] extends ControlObserver[Atom,Secti
           stack = addTree(CT.Fork(first, t), stack_)
         case _ => error(s"Entry mismatch, expected ForkSecond for $ev: $stack")
 
-  override def handle(ev: ExceptionControlEvent[Atom,Section,Exc,Fx]): Unit =
-    import ExceptionControlEvent.*
-    ev match
-      case BeginTry() =>
-        assertNoCatching()
-        stack = Entry.Try(CT.Empty()) :: stack
-      case Throw(exc: Exc) =>
-        assertNoCatching()
-        addTree(CT.Throw(exc))
-      case Catching() => stack match
-        case Entry.Try(t) :: stack_ =>
-          stack = Entry.Catching(t, List()) :: stack_
-        case _ => error(s"Entry mismatch, expected Try for $ev: $stack")
-      case BeginHandle(exc: Exc) => stack match
-        case Entry.Catching(body, hs) :: stack_ =>
-          stack = Entry.Handler(exc, CT.Empty()) :: Entry.Catching(body, hs) :: stack_
-        case _ => error(s"Entry mismatch, expected Catching for $ev: $stack")
-      case EndHandle() => stack match
-        case Entry.Handler(hx, t) :: Entry.Catching(body, hs) :: stack_ =>
-          stack = Entry.Catching(body, hs :+ (hx, t)) :: stack_
-        case _ => error(s"Entry mismatch, expected Handler for $ev: $stack")
-      case EndTry() => stack match
-        case Entry.Try(t) :: stack_ =>
-          stack = addTree(CT.Try(t, Nil), stack_)
-        case Entry.Catching(body, hs) :: stack_ =>
-          stack = addTree(CT.Try(body, hs), stack_)
-        case _ => error(s"Entry mismatch, expected Try or Catching for $ev: $stack")
+  private def addTree(t: CT, st: List[Entry]): List[Entry] = st match
+    case Entry.BaseTree(t0) :: st_ => Entry.BaseTree(CT.Seq(t0, t)) :: st_
+    case Entry.Sec(s, t0) :: st_ => Entry.Sec(s, CT.Seq(t0, t)) :: st_
+    case Entry.Try(t0) :: st_ => Entry.Try(CT.Seq(t0, t)) :: st_
+    case Entry.Handler(exc, t0, body, handlers) :: st_ => Entry.Handler(exc, CT.Seq(t0, t), body, handlers) :: st_
+    case Entry.ForkFirst(t0) :: st_ => Entry.ForkFirst(CT.Seq(t0, t)) :: st_
+    case Entry.ForkSecond(first, t0) :: st_ => Entry.ForkSecond(first, CT.Seq(t0, t)) :: st_
+    case Entry.Fixpoint(fx, t0) :: st_ => Entry.Fixpoint(fx, CT.Seq(t0, t)) :: st_
+    case Entry.Catching(_, _) :: st_ => error(s"Ill-formatted stack, top entry must not be Catching : $stack")
+    case Nil => error(s"Stack underflow: cannot add tree $t")
+    case _ => assert(false)
 
   override def handle(ev: FixpointControlEvent[Atom,Section,Exc,Fx]): Unit =
     import FixpointControlEvent.*
@@ -118,9 +118,9 @@ class ControlEventParser[Atom,Section,Exc,Fx] extends ControlObserver[Atom,Secti
     case Sec(s: Section, t: CT)
     case Try(t: CT)
     case Catching(body: CT, handlers: List[(Exc, CT)])
-    case Handler(exc: Exc, t: CT)
+    case Handler(exc: Exc, t: CT, body: CT, handlers: List[(Exc, CT)])
     case ForkFirst(t: CT)
-    case ForkSecond(first: CT, t: CT)
+    case ForkSecond(t1: CT, t2: CT)
     case Fixpoint(fx: Fx, t: CT)
 
     override def toString: String = this match
@@ -128,7 +128,7 @@ class ControlEventParser[Atom,Section,Exc,Fx] extends ControlObserver[Atom,Secti
       case Sec(s, t) => s"Sec($s, ${t.toString.take(10)}...)"
       case Try(t) => s"Try(${t.toString.take(10)}...)"
       case Catching(body, handlers) => s"Catching(${body.toString.take(10)}..., ${handlers.map((x, t) => s"$x -> ${t.toString.take(10)}...")})"
-      case Handler(exc, t) => s"Handler($exc, ${t.toString.take(10)}...)"
+      case Handler(exc, t, body, handlers) => s"Handler($exc, ${t.toString.take(10)}...)"
       case ForkFirst(t) => s"ForkFirst(${t.toString.take(10)}...)"
       case ForkSecond(first, t) => s"ForkSecond(${first.toString.take(10)}..., ${t.toString.take(10)}...)"
       case Fixpoint(fx, t) => s"Fixpoint($fx, ${t.toString.take(10)}...)"
