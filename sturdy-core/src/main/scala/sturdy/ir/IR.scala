c9cb8a59f208fc039bd00e4cd58988c60c6da73b
Sebastian Erdweg <seba.github@gmail.com>
2024-06-21 10:45:52 +0200

    adds IR.Select node

--- sturdy-core/src/main/scala/sturdy/ir/IR.scala
+++ sturdy-core/src/main/scala/sturdy/ir/IR.scala
@@ -9,18 +9,26 @@ enum IR:
   case Unknonwn()
   case Const[C](c: C)
   case Op(op: IROperator, args: Seq[IR])
+  case Select(cond: IR, left: IR, right: IR)
   case Join(left: IR, right: IR)
 
-  def predecessors: Seq[IR] = this match
+  override def hashCode(): Int = uid.hashCode()
+  override def equals(obj: Any): Boolean = obj match
+    case that: IR => this.uid == that.uid
+    case _ => false
+
+  def predecessors: Seq[(IR, String)] = this match
     case IR.Unknonwn() => Seq.empty
     case IR.Const(c) => Seq.empty
-    case IR.Op(op, args) => args
-    case IR.Join(left, right) => Seq(left, right)
+    case IR.Op(op, args) => args.zipWithIndex.map(a => a._1 -> a._2.toString)
+    case IR.Select(cond, left, right) => Seq(cond -> "?", left -> "⊤", right -> "⊥")
+    case IR.Join(left, right) => Seq(left -> "", right -> "")
 
   override def toString: String = this match
     case IR.Unknonwn() => s"Unknown@$uid"
     case IR.Const(c) => s"Const($c)@$uid"
     case IR.Op(op, args) => s"Op($op)@$uid"
+    case IR.Select(cond, left, right) => s"Select@$uid"
     case IR.Join(left, right) => s"Join@$uid"
 
 
@@ -29,9 +37,11 @@ object IR:
     IR.Op(op, arg +: args)
 
 
-given Join[IR] with
+class IRJoin extends Join[IR]:
   override def apply(v1: IR, v2: IR): MaybeChanged[IR] = MaybeChanged.Changed(IR.Join(v1, v2))
 
+class SelectJoin(cond: IR) extends Join[IR]:
+  def apply(v1: IR, v2: IR): MaybeChanged[IR] = MaybeChanged.Changed(IR.Select(cond, v1, v2))
 
 class IR_UID:
   override def toString: String = Integer.toHexString(hashCode)
