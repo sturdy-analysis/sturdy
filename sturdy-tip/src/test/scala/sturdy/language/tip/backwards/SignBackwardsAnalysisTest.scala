ad685b3213db944191f78ebd14e53a0179db6450
Stefan Marx <smarx01@students.uni-mainz.de>
2025-03-23 22:42:40 +0100

    minor addition to tests, backward-forward

--- sturdy-tip/src/test/scala/sturdy/language/tip/backwards/SignBackwardsAnalysisTest.scala
+++ sturdy-tip/src/test/scala/sturdy/language/tip/backwards/SignBackwardsAnalysisTest.scala
@@ -26,7 +26,8 @@ import sturdy.values.relational.{*, given}
 import sturdy.values.{*, given}
 import sturdy.*
 import sturdy.AbstractlySound
-import sturdy.language.tip.backward.{SignBackwardsAnalysis}
+import sturdy.language.tip.analysis.SignAnalysis
+import sturdy.language.tip.backward.SignBackwardsAnalysis
 import sturdy.language.tip.backward.SignBackwardsAnalysis.{*, given}
 
 import java.nio.file.{Files, Path, Paths}
@@ -59,7 +60,16 @@ class SignBackwardsAnalysisTest extends AnyFlatSpec, Matchers:
       runSignAnalysis(p, StackConfig.StackedStates())
     }
   }
-  
+
+  behavior of "BackwardForward SignAnalysis on MyTests"
+
+  Files.list(Paths.get(uriMyTests)).toScala(List).filter(p =>
+    p.getFileName.toString != "simple4.tip"
+  ).foreach { p =>
+    it must s"soundly analyze ${p.getFileName} with stacked states" in {
+      runBackwardForwardSignAnalysis(p, StackConfig.StackedStates())
+    }
+  }
 
 
 def runSignAnalysis(p: Path, stackConfig: StackConfig) =
@@ -73,7 +83,7 @@ def runSignAnalysis(p: Path, stackConfig: StackConfig) =
     if (functions.contains("main")) {
       //println(s"Program is ${program}")
       val analysis = new SignBackwardsAnalysis.Instance(Map(), Map(), stackConfig)
-//      val oldAnalysis = new oldBackward.SignBackwardsAnalysis.Instance(Map(), Map(), stackConfig)
+      val oldAnalysis = new oldBackward.SignBackwardsAnalysis.Instance(Map(), Map(), stackConfig)
 
       val inputs = () => ConcreteInterpreter.Value.IntValue(0)
       val interp = ConcreteInterpreter(Map(), Map(), () => ConcreteInterpreter.Value.IntValue(0))
@@ -84,42 +94,42 @@ def runSignAnalysis(p: Path, stackConfig: StackConfig) =
 
       println(s"Expected output of the program is ${cresult}")
 
-//      val oldaresult = oldAnalysis.failure.fallible(analysis.executeBack(program, expectedVal))
+      val oldaresult = oldAnalysis.failure.fallible(analysis.executeBack(program, expectedVal))
       val aresult = analysis.failure.fallible(analysis.executeBack(program, expectedVal))
 
-//      type C = CFallible[ConcreteInterpreter.Value]
-//      type A = AFallible[(Seq[Value], Value)]
-//
-//      given CAllocationIntIncrement[AllocationSite] = new CAllocationIntIncrement
-//      import sturdy.language.tip.backward.SignBackwardsAnalysisSoundness.{valuesAbstractly, addrAbstractly, po as poValue}
-//
-//      val abstractlyValue: Abstractly[ConcreteInterpreter.Value, Value] = valuesAbstractly(using addrAbstractly)
-//
-//      val numProgInputs = functions("main").params.size
-//      def abstractInputs(size: Int, input: () => ConcreteInterpreter.Value): Seq[Value] =
-//        0.until(size).map(_ => abstractlyValue(input()))
-//
-//      val abstractly: Abstractly[C, A] = {
-//        case failure.CFallible.Unfailing(t) => AFallible.Unfailing((abstractInputs(numProgInputs, inputs), abstractlyValue(t)))
-//        case failure.CFallible.Failing(kind, msg) => AFallible.Failing(Powerset((kind, msg)))
-//      }
-//      val poInputAndExpected: PartialOrder[(Seq[Value], Value)] = (x: (Seq[Value], Value), y: (Seq[Value], Value)) => (x, y) match
-//        case ((inputs1, expected1), (inputs2, expected2)) =>
-//          val inputOrder = inputs1.zip(inputs2).forall((i1, i2) => poValue.lteq(i1, i2))
-//          val expectedOrder = poValue.lteq(expected1, expected2)
-//          inputOrder && expectedOrder
-//      val po: PartialOrder[A] = (x: A, y: A) => (x, y) match
-//        case (AFallible.Diverging(_), _) => true
-//        case (AFallible.Unfailing(t1), AFallible.Unfailing(t2)) => poInputAndExpected.lteq(t1, t2)
-//        case (AFallible.Failing(fails1), AFallible.Failing(fails2)) => fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
-//        case (AFallible.Unfailing(t1), AFallible.MaybeFailing(t2, fails2)) => poInputAndExpected.lteq(t1, t2)
-//        case (AFallible.Failing(fails1), AFallible.MaybeFailing(t2, fails2)) => fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
-//        case (AFallible.MaybeFailing(t1, fails1), AFallible.MaybeFailing(t2, fails2)) => poInputAndExpected.lteq(t1, t2) && fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
-//        case _ => false
-//      val abstractlySound = AbstractlySound[C, A](using abstractly, po)
-//
-//      assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(cresult, aresult)(using abstractlySound))
-//      assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(interp, analysis))
+      type C = CFallible[ConcreteInterpreter.Value]
+      type A = AFallible[(Seq[Value], Value)]
+
+      given CAllocationIntIncrement[AllocationSite] = new CAllocationIntIncrement
+      import sturdy.language.tip.backward.SignBackwardsAnalysisSoundness.{valuesAbstractly, addrAbstractly, po as poValue}
+
+      val abstractlyValue: Abstractly[ConcreteInterpreter.Value, Value] = valuesAbstractly(using addrAbstractly)
+
+      val numProgInputs = functions("main").params.size
+      def abstractInputs(size: Int, input: () => ConcreteInterpreter.Value): Seq[Value] =
+        0.until(size).map(_ => abstractlyValue(input()))
+
+      val abstractly: Abstractly[C, A] = {
+        case failure.CFallible.Unfailing(t) => AFallible.Unfailing((abstractInputs(numProgInputs, inputs), abstractlyValue(t)))
+        case failure.CFallible.Failing(kind, msg) => AFallible.Failing(Powerset((kind, msg)))
+      }
+      val poInputAndExpected: PartialOrder[(Seq[Value], Value)] = (x: (Seq[Value], Value), y: (Seq[Value], Value)) => (x, y) match
+        case ((inputs1, expected1), (inputs2, expected2)) =>
+          val inputOrder = inputs1.zip(inputs2).forall((i1, i2) => poValue.lteq(i1, i2))
+          val expectedOrder = poValue.lteq(expected1, expected2)
+          inputOrder && expectedOrder
+      val po: PartialOrder[A] = (x: A, y: A) => (x, y) match
+        case (AFallible.Diverging(_), _) => true
+        case (AFallible.Unfailing(t1), AFallible.Unfailing(t2)) => poInputAndExpected.lteq(t1, t2)
+        case (AFallible.Failing(fails1), AFallible.Failing(fails2)) => fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
+        case (AFallible.Unfailing(t1), AFallible.MaybeFailing(t2, fails2)) => poInputAndExpected.lteq(t1, t2)
+        case (AFallible.Failing(fails1), AFallible.MaybeFailing(t2, fails2)) => fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
+        case (AFallible.MaybeFailing(t1, fails1), AFallible.MaybeFailing(t2, fails2)) => poInputAndExpected.lteq(t1, t2) && fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
+        case _ => false
+      val abstractlySound = AbstractlySound[C, A](using abstractly, po)
+
+      assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(cresult, aresult)(using abstractlySound))
+      assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(interp, analysis))
 
       println(s"Backward run of ${p.getFileName} ")
       aresult match
@@ -133,3 +143,75 @@ def runSignAnalysis(p: Path, stackConfig: StackConfig) =
       null
     }
 
+def runBackwardForwardSignAnalysis(p: Path, stackConfig: StackConfig) =
+  val file = Source.fromURI(p.toUri)
+  val sourceCode = file.getLines().mkString("\n")
+  file.close()
+  val program = Parser.parse(sourceCode)
+
+  val functions = program.funs.map(f => f.name -> f).toMap
+
+  if (functions.contains("main")) {
+    //println(s"Program is ${program}")
+    val analysis = new SignBackwardsAnalysis.Instance(Map(), Map(), stackConfig)
+    val oldAnalysis = new oldBackward.SignBackwardsAnalysis.Instance(Map(), Map(), stackConfig)
+
+    val oldaresult = oldAnalysis.failure.fallible(analysis.executeBack(program, Value.TopValue))
+    val aresult = analysis.failure.fallible(analysis.executeBack(program, Value.TopValue))
+
+
+    val inputs = () => ConcreteInterpreter.Value.IntValue(0)
+    val forwardAnalysis = new SignAnalysis.Instance(Map(), Map(), stackConfig)
+    val faresult = forwardAnalysis.failure.fallible(forwardAnalysis.execute(program))
+
+    println("LOOOK HERE _----------------------- " + faresult)
+
+//    val res = faresult.get.asInt(using forwardAnalysis.failure)
+//    val expectedVal = analysis.intOps.toValue(List(res))
+
+
+    type C = CFallible[ConcreteInterpreter.Value]
+    type A = AFallible[(Seq[Value], Value)]
+
+    given CAllocationIntIncrement[AllocationSite] = new CAllocationIntIncrement
+    import sturdy.language.tip.backward.SignBackwardsAnalysisSoundness.{valuesAbstractly, addrAbstractly, po as poValue}
+
+    val abstractlyValue: Abstractly[ConcreteInterpreter.Value, Value] = valuesAbstractly(using addrAbstractly)
+
+    val numProgInputs = functions("main").params.size
+    def abstractInputs(size: Int, input: () => ConcreteInterpreter.Value): Seq[Value] =
+      0.until(size).map(_ => abstractlyValue(input()))
+
+    val abstractly: Abstractly[C, A] = {
+      case failure.CFallible.Unfailing(t) => AFallible.Unfailing((abstractInputs(numProgInputs, inputs), abstractlyValue(t)))
+      case failure.CFallible.Failing(kind, msg) => AFallible.Failing(Powerset((kind, msg)))
+    }
+    val poInputAndExpected: PartialOrder[(Seq[Value], Value)] = (x: (Seq[Value], Value), y: (Seq[Value], Value)) => (x, y) match
+      case ((inputs1, expected1), (inputs2, expected2)) =>
+        val inputOrder = inputs1.zip(inputs2).forall((i1, i2) => poValue.lteq(i1, i2))
+        val expectedOrder = poValue.lteq(expected1, expected2)
+        inputOrder && expectedOrder
+    val po: PartialOrder[A] = (x: A, y: A) => (x, y) match
+      case (AFallible.Diverging(_), _) => true
+      case (AFallible.Unfailing(t1), AFallible.Unfailing(t2)) => poInputAndExpected.lteq(t1, t2)
+      case (AFallible.Failing(fails1), AFallible.Failing(fails2)) => fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
+      case (AFallible.Unfailing(t1), AFallible.MaybeFailing(t2, fails2)) => poInputAndExpected.lteq(t1, t2)
+      case (AFallible.Failing(fails1), AFallible.MaybeFailing(t2, fails2)) => fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
+      case (AFallible.MaybeFailing(t1, fails1), AFallible.MaybeFailing(t2, fails2)) => poInputAndExpected.lteq(t1, t2) && fails1.set.map(_._1).subsetOf(fails2.set.map(_._1))
+      case _ => false
+    val abstractlySound = AbstractlySound[C, A](using abstractly, po)
+
+//    assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(cresult, aresult)(using abstractlySound))
+//    assertResult(IsSound.Sound, p.getFileName)(Soundness.isSound(interp, analysis))
+
+    println(s"Backward run of ${p.getFileName} ")
+    aresult match
+      case AFallible.Unfailing(t) => println(s"  yields ${t._2} given arguments ${t._1}")
+      case AFallible.Failing(msgs) => println(s"  fails $msgs")
+      case AFallible.MaybeFailing(t, msgs) => println(s"  may yield ${t._2} given arguments ${t._1}, with old ${/*oldaresult.get._1*/} or fails $msgs")
+      case AFallible.Diverging(recur) => println(s"  diverges ($recur)")
+
+    (aresult, analysis)
+  } else {
+    null
+  }
