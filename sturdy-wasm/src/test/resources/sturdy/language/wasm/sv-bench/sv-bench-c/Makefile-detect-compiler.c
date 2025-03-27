e93a373dadb236cf207044ca44246fd52c9bb9e4
Sven Keidel <svenkeidel@pm.me>
2025-03-27 16:15:53 +0100

    sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Arrays.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/BitVectors.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/BitVectors-Termination.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Combinations.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Concurrency.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/ControlFlow.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/ControlFlow-Termination.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/CorrectnessWitnesses-Loops.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/ECA.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Floats.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Fuzzle.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Hardness.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Hardware.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Heap.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Heap-Termination.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Juliet.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/LinkedLists.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Loops.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/ProductLines.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Recursive.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Sequentialized.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-AWS-C-Common.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-BusyBox.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-coreutils.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-DeviceDriversLinux64Large.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-DeviceDriversLinux64.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-Intel-TDX-Module.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-OpenBSD.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-SQLite.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/SoftwareSystems-uthash.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Unused_Juliet.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/VerifyThis-Loops.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/VerifyThis-Recursive.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/ViolationWitnesses-ControlFlow.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/XCSP.set,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Makefile,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Makefile.config,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Makefile-detect-compiler.c,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Makefile-README.md,sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Makefile.rules: convert to Git LFS

--- sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Makefile-detect-compiler.c
+++ sturdy-wasm/src/test/resources/sturdy/language/wasm/sv-bench/sv-bench-c/Makefile-detect-compiler.c
@@ -1,17 +1,3 @@
-// This file is part of the SV-Benchmarks collection of verification tasks:
-// https://github.com/sosy-lab/sv-benchmarks
-//
-// SPDX-FileCopyrightText: 2015-2016 Daniel Liew <dan@su-root.co.uk>
-// SPDX-FileCopyrightText: 2015-2020 The SV-Benchmarks Community
-//
-// SPDX-License-Identifier: Apache-2.0
-
-// This file is used internally by the build system
-// to detect what compiler is being used.
-#if defined(__GNUC__) && !defined(__clang__)
-  SVCOMP_C_COMPILER_IS_GCC
-#elif defined(__GNUC__) && defined(__clang__)
-  SVCOMP_C_COMPILER_IS_CLANG
-#else
-#error Unknown C Compiler
-#endif
+version https://git-lfs.github.com/spec/v1
+oid sha256:4399250e9fc2097a114ce52dd9b1af5a85034a06b84e381f93ca3891bcbf2194
+size 581
