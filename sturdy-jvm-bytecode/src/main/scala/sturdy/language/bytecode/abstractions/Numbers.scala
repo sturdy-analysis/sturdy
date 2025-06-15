c61ed4f6892ca716ef16d8d57b9acb7e63c3b367
Joram Wolf <joram.wolf@stud.tu-darmstadt.de>
2025-06-15 18:59:26 +0200

    fix(numbers): fix warnings; cleanup

--- sturdy-jvm-bytecode/src/main/scala/sturdy/language/bytecode/abstractions/Numbers.scala
+++ sturdy-jvm-bytecode/src/main/scala/sturdy/language/bytecode/abstractions/Numbers.scala
@@ -1,11 +1,8 @@
 package sturdy.language.bytecode.abstractions
 
-import org.opalj.br.{ArrayType, ClassFile}
-import sturdy.language.bytecode.{ConcreteInterpreter, Interpreter}
-import sturdy.values.{Combine, MaybeChanged, Topped, Widening}
-import sturdy.values.objects.Object
-import sturdy.values.arrays.Array
 import sturdy.effect.failure.Failure
+import sturdy.language.bytecode.Interpreter
+import sturdy.values.Topped
 import sturdy.values.integer.NumericInterval
 
 trait Numbers extends Interpreter:
@@ -15,12 +12,14 @@ trait Numbers extends Interpreter:
   final type F64 = Topped[Double]
   final type Bool = Topped[Boolean]
 
-  
   final def topI32: I32 = Topped.Top
+
   final def topI64: I64 = Topped.Top
+
   final def topF32: F32 = Topped.Top
+
   final def topF64: F64 = Topped.Top
-  
+
   final def asBoolean(v: Value)(using Failure): Bool = v.asInt32 match
     case Topped.Top => Topped.Top
     case Topped.Actual(i) => Topped.Actual(i != 0)
@@ -36,17 +35,20 @@ trait IntervalNumbers extends Interpreter:
   final type F32 = Topped[Float]
   final type F64 = Topped[Double]
   final type Bool = Topped[Boolean]
-  
-  
+
   final def topI32: NumericInterval[Int] = NumericInterval(Int.MinValue, Int.MaxValue)
+
   final def topI64: NumericInterval[Long] = NumericInterval(Long.MinValue, Long.MaxValue)
+
   final def topF32: F32 = Topped.Top
+
   final def topF64: F64 = Topped.Top
 
   final def asBoolean(v: Value)(using failure: Failure): Bool = v match
     case Value.Int32(i) => i.toBoolean
     case Value.TopValue => Topped.Top
-  
+    case _ => ??? // TODO: not implemented
+
   final def boolean(b: Bool): Value = Value.Int32(b match
     case Topped.Top => NumericInterval(0, 1)
     case Topped.Actual(true) => NumericInterval(1, 1)
