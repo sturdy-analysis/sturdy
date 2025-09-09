6154e108ce5551fc1dd7e90e5a23faa7861a6e80
armandlego <alego@uni-mainz.de>
2025-09-09 09:23:10 +0200

    Small fixes for control events

--- sturdy-wasm/src/main/scala/sturdy/language/wasm/analyses/ConstantAnalysis.scala
+++ sturdy-wasm/src/main/scala/sturdy/language/wasm/analyses/ConstantAnalysis.scala
@@ -80,7 +80,8 @@ object ConstantAnalysis extends Interpreter, ConstantValues, ExceptionByTarget, 
       case ConcreteInterpreter.Value.Float32(f) => Value.Float32(Topped.Actual(f))
       case ConcreteInterpreter.Value.Float64(d) => Value.Float64(Topped.Actual(d))
 
-  class Instance(rootFrameData: FrameData, rootFrameValues: Iterable[Value], config: WasmConfig) extends GenericInstance:
+  class Instance(rootFrameData: FrameData, rootFrameValues: Iterable[Value], config: WasmConfig) extends
+    GenericInstance, ControlObservable[Control.Atom, Control.Section, Control.Exc, Control.Fx]:
     private given Instance = this
     
     var dummy: List[Value] = List()
