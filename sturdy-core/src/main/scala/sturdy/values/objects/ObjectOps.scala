96865a1bb7b832a159876e7c364b35ec7af99e70
Joram Wolf <joram.wolf@stud.tu-darmstadt.de>
2025-09-24 13:59:43 +0200

    chore(ObjectOps): cleanup; remove unused code

--- sturdy-core/src/main/scala/sturdy/values/objects/ObjectOps.scala
+++ sturdy-core/src/main/scala/sturdy/values/objects/ObjectOps.scala
@@ -1,23 +1,21 @@
 package sturdy.values.objects
 
-import org.apache.commons.math3.geometry.partitioning.BoundaryProjection
-import sturdy.data.MayJoin.WithJoin
-import sturdy.data.{JOption, JOptionA, JOptionC, MayJoin, NoJoin}
-import sturdy.effect.failure.{Failure, FailureKind}
-import sturdy.effect.store.Store
-import sturdy.effect.allocation.Allocator
+import sturdy.data.{JOption, MayJoin}
+import sturdy.effect.failure.Failure
 import sturdy.values.Structural
 import sturdy.values.ordering.EqOps
 
-import scala.collection.mutable
-
-
 trait ObjectOps[FieldIdentifier, ObjectIdentifier, Value, Class, ObjectValue, Site, Method, MthName, MthSig, Boolean, InvokeContext, J[_] <: MayJoin[_]]:
-  def makeObject(oid: ObjectIdentifier, c: Class, vals: Seq[(Value,Site,FieldIdentifier)]): ObjectValue
+  def makeObject(oid: ObjectIdentifier, c: Class, vals: Seq[(Value, Site, FieldIdentifier)]): ObjectValue
+
   def getField(callingClass: Class, obj: ObjectValue, identifier: FieldIdentifier)(using Failure): Value
+
   def setField(callingClass: Class, obj: ObjectValue, identifier: FieldIdentifier, v: Value): JOption[J, Unit]
+
   def invokeMethod(context: InvokeContext)(callingClass: Class, staticClass: Class, mthName: MthName, sig: MthSig, obj: ObjectValue, args: Seq[Value])(invoke: (ObjectValue, Method, Seq[Value]) => Value): Value
+
   def makeNull(): ObjectValue
+
   def isNull(obj: ObjectValue): Boolean
 
 
@@ -25,76 +23,8 @@ case class Object[OID, CF, FieldAddr, FieldName](oid: OID, cls: CF, fields: Map[
 
 given structuralObject[OID, CF, Addr, FieldName]: Structural[Object[OID, CF, Addr, FieldName]] with {}
 
-/*given ConcreteObjectOps[FieldAddr, FieldName, OID, V, Site, CF, Mth, MthName, MthSig]
-    (using alloc: Allocation[FieldAddr, Site], store: Store[FieldAddr, V, NoJoin]): ObjectOps[FieldName, OID, V, CF, Object[OID,CF,FieldAddr,FieldName], Object[OID,CF,FieldAddr,FieldName], Site, Mth, MthName, MthSig, Null, NoJoin] with
-  override def makeObject(oid: OID, cfs: CF, vals: Seq[(V,Site,FieldName)]): Object[OID, CF, FieldAddr, FieldName] =
-    val fieldAddrs = vals.map { (v, site, name) =>
-      val addr = alloc(site)
-      store.write(addr, v)
-      (name,addr)
-    }.toVector.toMap
-    Object(oid, cfs, fieldAddrs)
-
-  override def getField(obj: Object[OID, CF, FieldAddr, FieldName], name: FieldName): JOption[NoJoin, V] =
-    if (!obj.fields.contains(name))
-      JOptionC.none
-    else
-      store.read(obj.fields(name))
-
-  override def setField(obj: Object[OID, CF, FieldAddr, FieldName], name: FieldName, v: V): JOptionC[Unit] =
-    if (!obj.fields.contains(name))
-      JOptionC.none
-    else {
-      store.write(obj.fields(name), v)
-      JOptionC.some(())
-    }
-  override def invokeFunction(obj: Object[OID, CF, FieldAddr, FieldName], mth: Mth, args: Seq[V])
-                             (invoke: (Object[OID, CF, FieldAddr, FieldName], Mth, Seq[V]) => V): V =
-    invoke(obj, mth, args)
-
-  override def findFunction(obj: Object[OID, CF, FieldAddr, FieldName], name: MthName, sig: MthSig)
-                           (find: (Object[OID, CF, FieldAddr, FieldName], MthName, MthSig) => Mth): Mth =
-    find(obj, name, sig)
-
-  override def makeNull(): Null = null
-
-
-given ConcreteObjectOpsWithJoin[FieldAddr, FieldName, OID, V, Site, CF, Mth, MthName, MthSig]
-  (using fieldAlloc: Allocation[FieldAddr, Site], store: Store[FieldAddr, V, WithJoin]): ObjectOps[FieldName, OID, V, CF, Object[OID, CF, FieldAddr, FieldName], Object[OID, CF, FieldAddr, FieldName], Site, Mth, MthName, MthSig, Null, WithJoin] with
-    override def makeObject(oid: OID, cfs: CF, vals: Seq[(V, Site, FieldName)]): Object[OID, CF, FieldAddr, FieldName] =
-      val fieldAddrs = vals.map { (v, site, name) =>
-        val addr = fieldAlloc(site)
-        store.write(addr, v)
-        (name, addr)
-      }.toVector.toMap
-      Object(oid, cfs, fieldAddrs)
-
-    override def getField(obj: Object[OID, CF, FieldAddr, FieldName], name: FieldName): JOption[WithJoin, V] =
-      if (!obj.fields.contains(name))
-        JOptionA.none
-      else
-        store.read(obj.fields(name))
-
-    override def setField(obj: Object[OID, CF, FieldAddr, FieldName], name: FieldName, v: V): JOptionA[Unit] =
-      if (!obj.fields.contains(name))
-        JOptionA.none
-      else {
-        store.write(obj.fields(name), v)
-        JOptionA.some(())
-      }
-
-    override def invokeFunction(obj: Object[OID, CF, FieldAddr, FieldName], mth: Mth, args: Seq[V])
-                               (invoke: (Object[OID, CF, FieldAddr, FieldName], Mth, Seq[V]) => V): V =
-      invoke(obj, mth, args)
-
-    override def findFunction(obj: Object[OID, CF, FieldAddr, FieldName], name: MthName, sig: MthSig)
-                             (find: (Object[OID, CF, FieldAddr, FieldName], MthName, MthSig) => Mth): Mth =
-      find(obj, name, sig)
-
-    override def makeNull(): Null = null
-*/
 
 given ObjectEqOps[OID, CF, Addr, FieldName]: EqOps[Object[OID, CF, Addr, FieldName], Boolean] with
   override def equ(v1: Object[OID, CF, Addr, FieldName], v2: Object[OID, CF, Addr, FieldName]): Boolean = v1.oid == v2.oid
-  override def neq(v1: Object[OID, CF, Addr, FieldName], v2: Object[OID, CF, Addr, FieldName]): Boolean = v1.oid != v2.oid
 
+  override def neq(v1: Object[OID, CF, Addr, FieldName], v2: Object[OID, CF, Addr, FieldName]): Boolean = v1.oid != v2.oid
