8a194161cfd210f3e754eec60cc5d153e1be0731
Joram Wolf <joram.wolf@stud.tu-darmstadt.de>
2025-09-28 22:26:48 +0200

    feat(Failures): improve BytecodeFailure
    - better variant descriptions
    - every variant has a description
    - remove unnecessary variants

--- sturdy-jvm-bytecode/src/main/scala/sturdy/language/bytecode/generic/Failures.scala
+++ sturdy-jvm-bytecode/src/main/scala/sturdy/language/bytecode/generic/Failures.scala
@@ -4,18 +4,19 @@ import sturdy.effect.failure.FailureKind
 import sturdy.values.Finite
 
 enum BytecodeFailure extends FailureKind:
+  // failed to get/set local variable in frame
   case UnboundLocal
-  // field not assigned
+  // field missing from store
   case UnboundField
+  // static field missing from store
   case UnboundStaticVar
-  case IndexOutOfBounds
-  case MethodNotFound
+  // unexpected type
   case TypeError
   // a different variant of the Addr enum was expected
   case IncorrectAddrVariant
   // a different variant of the Site enum was expected
   case IncorrectSiteVariant
-  // the field does not exist
+  // field missing in a context where invariants dictate that it shouldn't
   case FieldNotFound
 
 given Finite[BytecodeFailure] with {}
