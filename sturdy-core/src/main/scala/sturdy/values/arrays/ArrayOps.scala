2bfdcdf6b13d01071b564318950eee822221f59e
Joram Wolf <joram.wolf@stud.tu-darmstadt.de>
2025-10-16 20:38:10 +0200

    chore(ArrayOps): formatting

--- sturdy-core/src/main/scala/sturdy/values/arrays/ArrayOps.scala
+++ sturdy-core/src/main/scala/sturdy/values/arrays/ArrayOps.scala
@@ -6,8 +6,11 @@ import sturdy.values.ordering.EqOps
 
 trait ArrayOps[ArrayIdentifier, Index, Value, ArrayValue, ArrayType, Site, ArrayOpContext, J[_] <: MayJoin[_]]:
   def makeArray(aid: ArrayIdentifier, valueSupplier: Int => (Value, Site), arrayType: ArrayType, arraySize: Index): ArrayValue
+
   def get(ctx: ArrayOpContext)(array: ArrayValue, idx: Index): JOption[J, Value]
+
   def set(ctx: ArrayOpContext)(array: ArrayValue, idx: Index, v: Value): JOption[J, Unit]
+
   def length(ctx: ArrayOpContext)(array: ArrayValue): Value
 
 case class Array[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize](aid: ArrayIdentifier, vals: Seq[ArrayElemAddr], arrayType: ArrayType, arraySize: ArraySize)
@@ -16,4 +19,5 @@ given structuralArray[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize]: Str
 
 given ArrayEqOps[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize]: EqOps[Array[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize], Boolean] with
   override def equ(v1: Array[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize], v2: Array[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize]): Boolean = v1.aid == v2.aid
+
   override def neq(v1: Array[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize], v2: Array[ArrayIdentifier, ArrayElemAddr, ArrayType, ArraySize]): Boolean = v1.aid != v2.aid
