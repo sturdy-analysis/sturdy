6deb55e5ac60b57863fa61c9497afa19f9160c94
Joram Wolf <joram.wolf@stud.tu-darmstadt.de>
2026-01-03 14:57:26 +0100

    fix(TypeOps): drop TypeOps object, ToppedSizeOps, IntervalSizeOps
    unused

--- sturdy-core/src/main/scala/sturdy/values/objects/TypeOps.scala
+++ sturdy-core/src/main/scala/sturdy/values/objects/TypeOps.scala
@@ -1,17 +1,10 @@
 package sturdy.values.objects
 
-import sturdy.values.Topped
-import sturdy.values.integer.NumericInterval
-
 trait TypeOps[V, TypeRep, B]:
   // TODO: is this made obsolete by ifInstanceOf?
   def instanceOf(v: V, target: TypeRep): B
   def typeOf(v: V): TypeRep
   def ifInstanceOf[A](v: V, ty: TypeRep)(ifTrue: () => A)(ifFalse: () => A): A
-
-object TypeOps:
-  def instanceOf[V, TypeRep, B](v: V, target: TypeRep)(using ops: TypeOps[V, TypeRep, B]): B =
-    ops.instanceOf(v, target)
 
 trait SizeOps[V, B]:
   def is32Bit(v: V): B
@@ -19,14 +12,3 @@ trait SizeOps[V, B]:
 object SizeOps:
   def is32Bit[V, B](v: V)(using ops: SizeOps[V, B]): B =
     ops.is32Bit(v)
-
-given ToppedSizeOps[V, B](using ops: SizeOps[V, B]): SizeOps[Topped[V], Topped[B]] with
-  override def is32Bit(v: Topped[V]): Topped[B] = v match
-    case Topped.Top => Topped.Top
-    case Topped.Actual(v) => Topped.Actual(ops.is32Bit(v))
-
-given IntervalSizeOps[V, B](using ops: SizeOps[V, B]): SizeOps[NumericInterval[V], Topped[B]] with
-  // TODO: implement
-  override def is32Bit(v: NumericInterval[V]): Topped[B] = ??? // v match
-//    case v: NumericInterval[Int] => Topped.Actual(ops.is32Bit(v.high))
-//    case v: NumericInterval[Long] => ???
