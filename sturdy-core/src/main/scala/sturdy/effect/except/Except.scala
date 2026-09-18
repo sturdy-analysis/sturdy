11dea8affa4e2303f99fbd624fcb82a661d37f80
armandlego <alego@uni-mainz.de>
2026-09-18 13:44:24 +0200

    Restore use of finally, but use try block to ignore raised exceptions

--- sturdy-core/src/main/scala/sturdy/effect/except/Except.scala
+++ sturdy-core/src/main/scala/sturdy/effect/except/Except.scala
@@ -2,7 +2,7 @@ package sturdy.effect.except
 
 import sturdy.data.JEither
 import sturdy.data.MayJoin
-import sturdy.effect.{Effect, EffectStack, SturdyException, TrySturdyFinally}
+import sturdy.effect.{Effect, EffectStack, SturdyException, SturdyThrowable}
 import sturdy.values.exceptions.Exceptional
 
 /** Effect [[Except]] causes and handles exceptions */
@@ -16,22 +16,13 @@ trait Except[Exc, E, J[_] <: MayJoin[_]] extends Effect, ObservableExcept[Exc]:
 
   final def tryCatch[A](f: => A)(handle: Exc => A): J[A] ?=> A =
     tryStart()
-    TrySturdyFinally { tries(TrySturdyFinally {f} {catchStart()}).either(identity) { e =>
+    try tries(try f finally { try catchStart() catch case e => println(e)}).either(identity) { e =>
       exceptional.handle(e) { exc =>
         handlingStart(exc)
-        TrySturdyFinally {handle(exc)} {handlingEnd()}
+        try handle(exc)
+        finally
+          try handlingEnd() catch case e => println(e)
       }
-    }} {
-      catchEnd()
-      tryEnd()
+    } finally {
+      try { catchEnd() ; tryEnd() } catch case e => println(e)
     }
-
-
-  final def tryFinally[A](f: => A)(g: => Unit): J[A] ?=> A =
-    val a = tryCatch(f)(exc => {g; throws(exc)})
-    g
-    a
-
-//  final def tryCatchFinally[A](f: => A)(handle: Exc => A)(g: => Unit): MayJoin[A] ?=> A =
-//    val tried = tries(f)
-//    tried.either(a => {g; a})(e => try exceptional.handle(e)(handle) finally g)
