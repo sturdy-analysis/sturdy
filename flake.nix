d9f5a8c61421bb73e5c2e15176551b1dba4a80c2
Hannes Greule <greule@kit.edu>
2026-09-21 13:26:13 +0200

    Initial nix-based compilation for CI

--- flake.nix
+++ flake.nix
@@ -12,72 +12,102 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, sbt }:
-    flake-utils.lib.eachDefaultSystem (sys:
-    let overlay = self: super: {
-            apron = self.callPackage sturdy-apron/apron.nix {};
-            elina = self.callPackage sturdy-apron/elina.nix {};
-            fenv = self.callPackage sturdy-apron/fenv.nix {};
-        };
-        pkgs = import nixpkgs {
-          system = sys;
-          overlays = [ overlay ];
-        };
-        jdk = pkgs.jdk21_headless;
-    in {
-      packages = rec {
-        pyenv = pkgs.python3.withPackages (ps: with ps; [
-          jupyter
-          ipython
-          pandas
-          seaborn
-        ]);
-        apron = pkgs.apron;
-        elina = pkgs.elina;
-        fenv = pkgs.fenv;
-        numerical-analysis-libraries = pkgs.buildEnv {
-          name = "numerical-analysis-libraries";
-          paths = [
-            apron
+    let
+      forAllSystems = nixpkgs.lib.genAttrs flake-utils.lib.defaultSystems;
+      perSystem = system:
+        let
+          overlay = final: prev: {
+            apron = final.callPackage sturdy-apron/apron.nix {};
+            elina = final.callPackage sturdy-apron/elina.nix {};
+            fenv = final.callPackage sturdy-apron/fenv.nix {};
+          };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
+          jdk = pkgs.jdk21_headless;
+          numerical-analysis-libraries = pkgs.buildEnv {
+            name = "numerical-analysis-libraries";
+            paths = [
+              pkgs.apron
 #            elina doesn't build on mac
-            fenv
-          ];
-        };
-        sturdy = sbt.lib.mkSbtDerivation {
-          pkgs = pkgs;
-          pname = "sturdy";
-          version = "0.1";
-          src = ./.;
-          depsWarmupCommand = ''
+              pkgs.fenv
+            ];
+          };
+          ciCompileScript = pkgs.writeShellScriptBin "ci-compile" ''
+            set -euo pipefail
             rm -rf sturdy-apron/lib
             ln -s ${numerical-analysis-libraries}/lib sturdy-apron/lib
             sbt compile
           '';
-          nativeBuildInputs = [ numerical-analysis-libraries ];
-          depsSha256 = "sha256-ZllRXxIE6qomVoRj0t8pBPLH9sslLUmU9Dxc6pv0eew=";
+        in {
+          apps = rec {
+            ci-compile = {
+              type = "app";
+              program = "${ciCompileScript}/bin/ci-compile";
+            };
+            default = ci-compile;
+          };
+          devShells = {
+            default = pkgs.mkShell {
+              packages = [ jdk pkgs.sbt numerical-analysis-libraries ciCompileScript ];
+            };
+            ci = pkgs.mkShell {
+              packages = [ jdk pkgs.sbt numerical-analysis-libraries ciCompileScript ];
+            };
+          };
+          packages = rec {
+            pyenv = pkgs.python3.withPackages (ps: with ps; [
+              jupyter
+              ipython
+              pandas
+              seaborn
+            ]);
+            apron = pkgs.apron;
+            elina = pkgs.elina;
+            fenv = pkgs.fenv;
+            inherit numerical-analysis-libraries;
+            sturdy = sbt.lib.mkSbtDerivation {
+              pkgs = pkgs;
+              pname = "sturdy";
+              version = "0.1";
+              src = ./.;
+              depsWarmupCommand = ''
+                rm -rf sturdy-apron/lib
+                ln -s ${numerical-analysis-libraries}/lib sturdy-apron/lib
+                sbt compile
+              '';
+              nativeBuildInputs = [ numerical-analysis-libraries ];
+              depsSha256 = "sha256-ZllRXxIE6qomVoRj0t8pBPLH9sslLUmU9Dxc6pv0eew=";
 
-          buildPhase = ''
-            rm -rf sturdy-apron/lib
-            ln -s ${numerical-analysis-libraries}/lib sturdy-apron/lib
-            sbt sturdy_wasm/Test/assembly
-          '';
+              buildPhase = ''
+                rm -rf sturdy-apron/lib
+                ln -s ${numerical-analysis-libraries}/lib sturdy-apron/lib
+                sbt sturdy_wasm/Test/assembly
+              '';
 
-          installPhase = ''
-            mkdir -p $out/sturdy
-            cp -r ./* $out/sturdy/
-          '';
-        };
-        docker = pkgs.dockerTools.buildLayeredImage {
-          name = "sturdy";
-          tag = "latest";
-          contents = [
-            sturdy jdk pkgs.bash pkgs.coreutils pkgs.busybox
-          ];
-          config = {
-            Env = [ "PATH=/bin:${pkgs.bash}/bin:${pkgs.coreutils}/bin:${pkgs.busybox}/bin" ];
-            Cmd = "${pkgs.bash}/bin/bash";
-            WorkingDir = "/sturdy/";
+              installPhase = ''
+                mkdir -p $out/sturdy
+                cp -r ./* $out/sturdy/
+              '';
+            };
+            docker = pkgs.dockerTools.buildLayeredImage {
+              name = "sturdy";
+              tag = "latest";
+              contents = [
+                sturdy jdk pkgs.bash pkgs.coreutils pkgs.busybox
+              ];
+              config = {
+                Env = [ "PATH=/bin:${pkgs.bash}/bin:${pkgs.coreutils}/bin:${pkgs.busybox}/bin" ];
+                Cmd = "${pkgs.bash}/bin/bash";
+                WorkingDir = "/sturdy/";
+              };
+            };
           };
         };
-      };
-    });
+    in {
+      apps = forAllSystems (system: (perSystem system).apps);
+      devShells = forAllSystems (system: (perSystem system).devShells);
+      packages = forAllSystems (system: (perSystem system).packages);
+    };
 }
